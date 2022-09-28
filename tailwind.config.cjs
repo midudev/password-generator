83cbe97181856742ea39c45175f108a8db243b75
Miguel Ángel Durán <miduga@gmail.com>
2022-09-28 15:33:03 -0500

    feat: improve design for the website

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				spark: '#ffe27d',
+				surf: '#64e2ff',
+				psybeam: '#9092ff',
+				giga: '#b4ff39',
+				manga: '#e5e1e6'
+			}
+		},
+		fontFamily: {
+			sans: ['Jet Brains Mono', 'monospace'],
+			special: ['Orbitron', 'sans-serif']
+		}
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/typography')]
 }
