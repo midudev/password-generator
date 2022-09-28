e54ae181550b317f4ea60855aa5a55ee9849259d
Miguel Ángel Durán <miduga@gmail.com>
2022-09-28 12:57:58 -0500

    build: stop using Lit as it has problems with static rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,24 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
-import react from '@astrojs/react';
-import vue from '@astrojs/vue';
-import solidJs from '@astrojs/solid-js';
-import lit from '@astrojs/lit';
-import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
-import analogjsangular from '@analogjs/astro-angular';
+import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
+import react from '@astrojs/react'
+import vue from '@astrojs/vue'
+import solidJs from '@astrojs/solid-js'
+import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image'
+import analogjsangular from '@analogjs/astro-angular'
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), react(), vue(), solidJs(), lit(), tailwind(), image(), analogjsangular(), preact()]
-});
+	integrations: [
+		svelte(),
+		react(),
+		vue(),
+		solidJs(),
+		tailwind(),
+		image(),
+		analogjsangular(),
+		preact()
+	]
+})
