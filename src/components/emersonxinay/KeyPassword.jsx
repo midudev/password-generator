48dd8b010bc062715ed26463332ebc2508b1f5b6
Emerson Espinoza <xinayespinoza@gmail.com>
2022-10-01 19:47:04 -0300

    mi estructura base

---
+++ src/components/emersonxinay/KeyPassword.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export const KeyPassword = () => {
+	return (
+
+		<>
+			<div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-blue-600'>
+				<h4 className='text-white'>Aquí se mostrara pronto mi solución</h4>
+			</div>
+		</>
+	)
+}
