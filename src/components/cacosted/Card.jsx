c6521d84829e0fa76824e21b8bd56435977bd6c3
Clemente Acosta <clems.acosta@gmail.com>
2022-10-01 13:44:55 -0500

    Add usePassword hook, Header and CopyIcon components

--- src/components/cacosted/Card.jsx
+++ src/components/cacosted/Card.jsx
@@ -1,17 +1,67 @@
-import { useState } from "react"
+
+import React, { useEffect, useState } from "react"
+import { CopyIcon } from "@components/cacosted/CopyIcon.jsx"
+import { Header } from "@components/cacosted/Header.jsx"
+const CHARSET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-='
+
+const usePassword = (initialLength = 5) => {
+	const [passLength, setPassLength] = useState(initialLength)
+	const [password, setPassword] = useState('')
+
+	const getRandomChar = () => {
+		const charArray = CHARSET.split('')
+		const randomIndex = Math.floor(Math.random() * CHARSET.length)
+		const randomChar = charArray[randomIndex]
+		return randomChar
+	}
+
+	const generatePassword = () => {
+		const password = Array.from({length: passLength}).map(() => getRandomChar())
+		return password.join('')
+	}
+
+	const setNewLength = (number) => setPassLength(number)
+	
+	const setNewPassword = (string) => setPassword(string)
+	
+
+	useEffect(() => {
+		const newPassword = generatePassword()
+		setNewPassword(newPassword)
+	},[passLength])
+
+	return {
+		password,
+		setNewLength
+	}
+}
 
 export const Card = () => {
-	const [passLength, setPassLength] = useState(0)
-
+	
+	const {password, setNewLength} = usePassword()
 	const handleChange = ({target}) => {
-		setPassLength(target.value)
+		setNewLength(target.value)
 	}
+	
 	return (
-		<main class="text-white flex justify-center items-center">
-			<article class="bg-yellow-400 rounded-xl">
-				<h1 class="font-bold text-2xl">Password Generator</h1>
-				<input onChange={handleChange} type="range" name="password-length" id="password-length" min="0" max="20" value={passLength}/>
-				<span class="block bg-slate-400 p-5">{passLength}</span>
+		<main className="text-white flex justify-center items-center">
+			<article className="w-3/5 bg-gray-900 rounded-xl flex flex-col gap-4 p-5">
+				<Header />
+				<label htmlFor="password" className="relative block bg-green-400">
+					<input name="password" id="password" className="text-slate-400 w-full px-4 py-2" type="text" readOnly value={password}/>
+					<CopyIcon fill="red" className="absolute top-0 right-0"/>
+				</label>
+				<label htmlFor="password-length">
+					<span className="block bg-slate-400 px-4 py-2">{password.length}</span>
+					<input 
+						onChange={handleChange} 
+						type="range" 
+						name="password-length" 
+						id="password-length" 
+						min="1" 
+						max="100" 
+						value={password.length}/>
+				</label>
 				<button>Generate</button>
 			</article>
 		</main>
---
+++ src/components/cacosted/Header.jsx
@@ -0,0 +1,4 @@
+import React from "react"
+export const Header = () => {
+	return <h1 className="font-bold text-2xl">Password Generator</h1>
+}
