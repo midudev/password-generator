62e25894d25c25eff56fe80df6f5f8506ac301aa
@CamiloArias47 <camillo47@gmail.com>
2022-10-01 17:35:24 -0500

    add CamiloArias47 page 🗏

---
+++ src/components/CamiloArias47/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import "./styles/app-styles.css"
+
+export default function App(){
+	return (
+		<div className='app'>
+			<section>
+				<h1>Random Generator</h1>
+				<button>
+					Generate Password
+				</button>
+			</section>
+		</div>
+	)
+}
