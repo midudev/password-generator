3e28117b9d0ce53daed4060ac5c31765c4b94b39
Miguel Angel Abad <miguel.abad86@gmail.com>
2022-10-02 20:42:50 +0200

    style(zerok1986): add styles to button and icon to copy button

--- src/components/zerok1986/App.jsx
+++ src/components/zerok1986/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
 		<div className='text-white'>
 			<div id='password-generator'>
 				<input type='text' id='password' />
-				<button id='copy'>Copy</button>
+				<button id='copy'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M0 0h24v24H0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/></svg></button>
 				<div id='settings'>
 					<label htmlFor='length'>Length</label>
 					<input 
