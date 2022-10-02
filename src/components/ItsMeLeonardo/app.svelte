c0971c9e60e7fed4fcb9033c33ae247a4387a44f
itsmeleonardo <carlosgamercovi20@gmail.com>
2022-10-02 18:03:30 -0500

    complete mobile view

---
+++ src/components/ItsMeLeonardo/app.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import Title from '@components/ItsMeLeonardo/components/Title.svelte'
+	import Section from '@components/ItsMeLeonardo/components/Section.svelte'
+	import RangeField from '@components/ItsMeLeonardo/components/RangeField.svelte'
+	import ToggleField from '@components/ItsMeLeonardo/components/ToggleField.svelte'
+
+	import Button from '@components/ItsMeLeonardo/components/Button.svelte'
+
+	let passwordLength = 16
+</script>
+
+<main class="container p-2">
+	<form class="mx-auto w-full h-full max-w-[500px] flex items-center flex-col gap-4">
+		<Title>Password Generator</Title>
+		<Section>
+			<h6 slot="title">Generated password</h6>
+			<div class="w-full bg-purple-900/50">
+				<p class="w-full px-4 py-3 text-neutral-200 text-center">afñakfña9878ñ</p>
+			</div>
+		</Section>
+
+		<Section>
+			<span slot="title">Length: {passwordLength}</span>
+			<RangeField bind:value={passwordLength} />
+		</Section>
+
+		<Section>
+			<span slot="title">Settings</span>
+			<ToggleField>
+				<span slot="label">Include numbers</span>
+			</ToggleField>
+
+			<ToggleField>
+				<span slot="label">Include capital letters</span>
+			</ToggleField>
+
+			<ToggleField>
+				<span slot="label">Include special characters</span>
+			</ToggleField>
+		</Section>
+
+		<Button>Generate password</Button>
+	</form>
+</main>
---
+++ src/components/ItsMeLeonardo/components/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let label: string = ''
+</script>
+
+<button class="w-full cursor-pointer py-3 bg-gradient-to-r from-purple-700 to-purple-900">
+	<span class="text-slate-200">
+		<slot>{label}</slot>
+	</span>
+</button>
