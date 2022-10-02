e0eb45c9a2e28dd51843b823bed3f06fd28f24e5
Miguel Angel Abad <miguel.abad86@gmail.com>
2022-10-02 19:37:16 +0200

    feat(zerok1986): add evt listener and onChange function to handle password length

--- src/components/zerok1986/App.jsx
+++ src/components/zerok1986/App.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 export default function App() {
-  return (
-	<div class='box'>
-		<div class='text-white'>
+	const [passLength, setPassLength] = useState(8)
+	return (
+	<div className='box'>
+		<div className='text-white'>
 			<div id='password-generator'>
 				<input type='text' id='password' />
 				<button id='copy'>Copy</button>
 				<div id='settings'>
-					<label for='length'>Length</label>
-					<input type='range' id='length' value='8' min='6' max='24' />
-					<p id='value'></p>
+					<label htmlFor='length'>Length</label>
+					<input 
+						type='range' 
+						id='length' 
+						value={passLength} 
+						onChange={(e) => {
+							setPassLength(e.target.value)
+						}}
+						min='6' 
+						max='24'
+						step='1' 
+						/>
+					<span id='value'>{passLength}</span>
 				</div>
 				<button id='generate'>Generate Password</button>
 			</div>
