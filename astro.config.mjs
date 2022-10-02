fa4c52cb928b63b56db64255e36aa51c773bd43e
Luis Marsiglia <marsiglia.business@gmail.com>
2022-10-02 17:30:56 -0400

    feat: add preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,9 @@ import vue from '@astrojs/vue'
 import tailwind from '@astrojs/tailwind'
 import image from '@astrojs/image'
 import solidJs from '@astrojs/solid-js'
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), react(), vue(), tailwind(), image(), solidJs()]
+	integrations: [svelte(), react(), vue(), tailwind(), image(), solidJs(), preact()]
 })
