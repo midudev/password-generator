b83c4335c001c6e98e8744eab1fe1df1b9219e5a
Miguel Angel Abad <miguel.abad86@gmail.com>
2022-10-02 22:11:02 +0200

    style(zerok1986): minor styling changes to make it responsive

--- src/components/zerok1986/App.jsx
+++ src/components/zerok1986/App.jsx
@@ -24,33 +24,31 @@ export default function App() {
 	}
 
 	return (
-	<div className='box text-white'>
-		<div>
-			<div id='password-generator'>
-				<div id='password-box'>
-					<p id='password-text' className='text-white'>{password}</p>
-					<button id='copyButton' onClick={copyToClipboard}>
-						<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff">
-							<path d="M0 0h24v24H0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/>
-						</svg>
-					</button>
-				</div>
-				<div id='settings'>
-					<label htmlFor='length'>Length</label>
-					<input 
-						type='range'
-						id='length' 
-						value={passLength} 
-						onChange={(e) => {
-							setPassLength(e.target.value)
-						}}
-						min='6' 
-						max='24'
-						step='1' 
-						/>
-					<span id='value'>{passLength}</span>
-					<button id='generate' onClick={() => generatePassword(passLength)}>Generate Password</button>
-				</div>
+	<div id='box'>
+		<div id='password-generator'>
+			<div id='password-box'>
+				<p id='password-text'>{password}</p>
+				<button id='copyButton' onClick={copyToClipboard}>
+					<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff">
+						<path d="M0 0h24v24H0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/>
+					</svg>
+				</button>
+			</div>
+			<div id='settings'>
+				<label htmlFor='length'>Length</label>
+				<input 
+					type='range'
+					id='length' 
+					value={passLength} 
+					onChange={(e) => {
+						setPassLength(e.target.value)
+					}}
+					min='6' 
+					max='24'
+					step='1' 
+					/>
+				<span id='value'>{passLength}</span>
+				<button id='generate' onClick={() => generatePassword(passLength)}>Generate Password</button>
 			</div>
 		</div>
 	</div>
