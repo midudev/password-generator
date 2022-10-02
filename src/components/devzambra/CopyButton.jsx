2caa3f20a80d06f7332b9427befae0c894579ce5
devzambra <dev.mzambrano@gmail.com>
2022-10-02 16:47:17 +0200

    feat(devzambra): add password generator in devzambra page

---
+++ src/components/devzambra/CopyButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ClipboardIcon from './icons/ClipboardIcon'
+import styles from './styles/neumorphism.module.css'
+
+function CopyButton ({ handleCopy, disabled }) {
+	return (
+		<button onClick={handleCopy} className={`p-4 rounded-full bg-gray-100 ${styles.neuShadow}`} disabled={disabled}>
+			<ClipboardIcon width={24} height={24} fill={'black'} />
+		</button>
+	)
+}
+
+export default CopyButton
