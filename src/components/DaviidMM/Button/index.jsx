5065efc6288cc6878ca1297843f9a95c9eefad03
DaviidMM <davicitoo1612@gmail.com>
2022-10-02 12:48:55 +0200

    refactor(tailwind): use arbitrary classes and remove tailwind config changes

--- src/components/DaviidMM/Button/index.jsx
+++ src/components/DaviidMM/Button/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const colorClasses = {
-	violet: 'bg-gradient-to-br from-purple-400 via-purple-500 to-purple-700 bg-size-200 bg-pos-0 hover:bg-pos-100 hover:text-white transition-all duration-500'
+	violet: 'bg-gradient-to-br from-purple-400 via-purple-500 to-purple-700 bg-[length:200%_200%] bg-[position:0%_0%] hover:bg-[position:100%_100%] hover:text-white transition-all duration-500'
 }
 
 const Button = ({ children, className, color = 'violet', onClick }) => {
