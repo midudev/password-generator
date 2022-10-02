23777247fdd565d31ab49c7e584258b3785cdaef
itsmeleonardo <carlosgamercovi20@gmail.com>
2022-10-02 18:49:31 -0500

    fix:click event propagation for buttons

--- src/components/ItsMeLeonardo/components/Button.svelte
+++ src/components/ItsMeLeonardo/components/Button.svelte
@@ -2,7 +2,7 @@
 	export let label: string = ''
 </script>
 
-<button class="w-full cursor-pointer py-3 bg-gradient-to-r from-purple-700 to-purple-900">
+<button class="w-full cursor-pointer py-3 bg-gradient-to-r from-purple-700 to-purple-900" on:click>
 	<span class="text-slate-200">
 		<slot>{label}</slot>
 	</span>
