c456b7341ea858c4aebb5cb5dac335d64a7ad8b9
Adam El Amrani <adelamco1091@outlook.com>
2022-10-02 09:23:45 +0200

    Change styles, create utils.js

--- src/components/adamelamrani/PasswordGenerator.jsx
+++ src/components/adamelamrani/PasswordGenerator.jsx
@@ -1,9 +1,16 @@
 import ButtonComponent from './ButtonComponent'
+import './styles/passwordGenerator.css'
 
 const PasswordGenerator = () => {
 	return (
 		<div>
 			<h1>Generate a random password</h1>
+			<h2>Password length</h2>
+			<div className='input-range-box'>
+				<p>6</p>
+				<input type='range' min='6' max='30' value='6' />
+				<p>30</p>
+			</div>
 			<ButtonComponent text='Generate password' onClick={() => {}} />
 		</div>
 	)
