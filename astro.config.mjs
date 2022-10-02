4cfddddb847f8bbd945b293d88f930567ad7e13c
Miguel Ángel Durán <miduga@gmail.com>
2022-10-02 19:10:22 +0200

    Remove Solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import react from '@astrojs/react'
 import vue from '@astrojs/vue'
-import solidJs from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
 import image from '@astrojs/image'
 import analogjsangular from '@analogjs/astro-angular'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), react(), vue(), solidJs(), tailwind(), image(), analogjsangular()]
+	integrations: [svelte(), react(), vue(), tailwind(), image(), analogjsangular()]
 })
