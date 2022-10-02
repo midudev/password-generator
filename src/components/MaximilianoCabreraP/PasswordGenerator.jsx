f21f93dcfd985e79405de47f5744301cc973099e
Maximiliano Cabrera <39493720+MaximilianoCabreraP@users.noreply.github.com>
2022-10-02 00:34:36 -0300

    First working commit

--- src/components/MaximilianoCabreraP/PasswordGenerator.jsx
+++ src/components/MaximilianoCabreraP/PasswordGenerator.jsx
@@ -1,25 +1,64 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
+const MAYUSCULAS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const MINUSCULAS = 'abcdefghijklmnopqrstuvwxyz'
+const NUMEROS = '0123456789'
+const SIMBOLOS = '!@#$%^&*_-+='
 export const PasswordGenerator = () => {
 	const [passwordLength, setPasswordLength] = useState(8)
-	console.log({ passwordLength });
+	const [filters, setFilters] = useState({
+		uppercase: true,
+		lowercase: false,
+		numbers: false,
+		symbols: false
+	})
+	const [password, setPassword] = useState('')
 
-	function handleChangeLength (e) {
-		console.log("---")
+	function handleChangeLength ({ target }) {
+		setPasswordLength(target.value)
+	}
 
+	useEffect(() => {
+		generatePassword()
+	}, [passwordLength, filters])
+
+	function generatePassword () {
+		let chars = ''
+		chars += filters.uppercase ? MAYUSCULAS : ''
+		chars += filters.lowercase ? MINUSCULAS : ''
+		chars += filters.numbers ? NUMEROS : ''
+		chars += filters.symbols ? SIMBOLOS : ''
+
+		let randomstring = ''
+		for (let i = 0; i < passwordLength; i++) {
+			const randomNumber = Math.floor(Math.random() * chars.length)
+			randomstring += chars.substring(randomNumber, randomNumber + 1)
+		}
+		setPassword(randomstring)
 	}
 
 	return (
-		<div className="grid justify-items-center  gap-2">
+		<div className='grid justify-items-center'>
 			<ul>Agregar:
-				<li>Mayusculas: <input type='checkbox' name='uppercase' /></li>
-				<li>Minúsculas: <input type='checkbox' name='lowercase' /></li>
-				<li>Números: <input type='checkbox' name='numbers' /></li>
-				<li>Símbolos: <input type='checkbox' name='symbols' /></li>
+				<li>Mayusculas: <input type='checkbox' defaultChecked={filters.uppercase} name='uppercase' onClick={() => setFilters({ ...filters, uppercase: !filters.uppercase })}/></li>
+				<li>Minúsculas: <input type='checkbox' defaultChecked={filters.lowercase} name='lowercase' onClick={() => setFilters({ ...filters, lowercase: !filters.lowercase })}/></li>
+				<li>Números: <input type='checkbox' defaultChecked={filters.numbers} name='numbers' onClick={() => setFilters({ ...filters, numbers: !filters.numbers })}/></li>
+				<li>Símbolos: <input type='checkbox' defaultChecked={filters.symbols} name='symbols' onClick={() => setFilters({ ...filters, symbols: !filters.symbols })}/></li>
 			</ul>
 
-			<span><input type='range' name='length' min='8' max='64' onChange={handleChangeLength()} />{passwordLength}</span>
-			<button className="bg-sky-500 hover:bg-sky-700 rounded-md p-1 ">Generate</button>
+			<span>
+				8 <input
+					max='64'
+					min='8'
+					onChange={handleChangeLength}
+					type='range'
+					value={passwordLength}
+				/> 64
+			</span>
+			Selected {passwordLength}
+			<span>Password: {password || 'por favor seleccioná al menos un filtro'}</span>
+			<button className='bg-sky-500 hover:bg-sky-700 rounded-md p-1' onClick={generatePassword}
+			>Generate</button>
 		</div>
 	)
 }
