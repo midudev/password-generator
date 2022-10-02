db89650e5dff0d3bc0d096c3d41b63e075bf5d07
Adam El Amrani <adelamco1091@outlook.com>
2022-10-02 08:33:09 +0200

    Create PasswordGenerator component

---
+++ src/components/adamelamrani/PasswordGenerator.jsx
@@ -0,0 +1,12 @@
+import ButtonComponent from './ButtonComponent'
+
+const PasswordGenerator = () => {
+	return (
+		<div>
+			<h1>Generate a random password</h1>
+			<ButtonComponent text='Generate password' onClick={() => {}} />
+		</div>
+	)
+}
+
+export default PasswordGenerator
