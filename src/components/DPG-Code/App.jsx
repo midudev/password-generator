faf9b351b19293ead10c3aaccb673b22b4ea596a
DPG Code <prietogaviriadaniel@gmail.com>
2022-10-03 09:35:25 +0200

    feat: solution dpg code

---
+++ src/components/DPG-Code/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from './components/Form'
+import Instructions from './components/Instructions'
+import './index.css'
+
+function App () {
+  return (
+    <main className='m-auto py-10 flex flex-col gap-6 items-center justify-center   md:py-16 md:flex-row md:gap-12 md:items-start   2xl:py-32 2xl:gap-24'>
+      <Instructions />
+      <Form />
+    </main>
+  )
+}
+export default App
