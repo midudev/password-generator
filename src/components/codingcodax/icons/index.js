39defc99a692df7bc1c8fa909b077b9029d34978
Alexis Guzman <alexis12989x@gmail.com>
2022-10-03 14:03:49 -0500

    feat(components): export icons

---
+++ src/components/codingcodax/icons/index.js
@@ -0,0 +1,4 @@
+export { default as CopyIcon } from './Copy'
+export { default as CopySuccessIcon } from './CopySuccess'
+export { default as EyeIcon } from './Eye'
+export { default as RefreshIcon } from './Refresh'
