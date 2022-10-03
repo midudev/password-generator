0d755922bcb561ba6a335956476c80a29c5ec0bb
Alexis Guzman <alexis12989x@gmail.com>
2022-10-03 14:15:43 -0500

    feat(components): create and setup generate password button component

---
+++ src/components/codingcodax/GeneratePassowordButton.jsx
@@ -0,0 +1,14 @@
+import { RefreshIcon } from '@components/codingcodax/icons'
+
+const GeneratePasswordButton = ({ handleChange }) => {
+	return (
+		<button
+			className='py-4 px-6 w-full flex items-center justify-center gap-4 text-indigo-100 bg-indigo-600 rounded-xl duration-200 hover:bg-indigo-700 active:scale-90'
+			onClick={handleChange}
+		>
+			Generate Password <RefreshIcon className='stroke-indigo-100 w-5 h-5' />
+		</button>
+	)
+}
+
+export default GeneratePasswordButton
