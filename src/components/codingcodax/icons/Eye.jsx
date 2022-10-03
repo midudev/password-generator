4f9ca7792bd80761fbb3a56a0d02afec512e716e
Alexis Guzman <alexis12989x@gmail.com>
2022-10-03 14:03:12 -0500

    feat(components): create eye icon

---
+++ src/components/codingcodax/icons/Eye.jsx
@@ -0,0 +1,20 @@
+const Eye = ({ className }) => {
+	return (
+		<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' className={className}>
+			<path
+				d='M15.58 12c0 1.98-1.6 3.58-3.58 3.58S8.42 13.98 8.42 12s1.6-3.58 3.58-3.58 3.58 1.6 3.58 3.58Z'
+				stroke-width='1.5'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+			/>
+			<path
+				d='M12 20.27c3.53 0 6.82-2.08 9.11-5.68.9-1.41.9-3.78 0-5.19-2.29-3.6-5.58-5.68-9.11-5.68-3.53 0-6.82 2.08-9.11 5.68-.9 1.41-.9 3.78 0 5.19 2.29 3.6 5.58 5.68 9.11 5.68Z'
+				stroke-width='1.5'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+			/>
+		</svg>
+	)
+}
+
+export default Eye
