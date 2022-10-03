11febfa23cd3b15664e67b70c6f3da4b1ba75ad1
Vallel Blanco <vallelblanco@gmail.com>
2022-10-02 22:10:14 -0700

    Wrap copied password message

--- src/components/vallel/PasswordGenerator.jsx
+++ src/components/vallel/PasswordGenerator.jsx
@@ -28,12 +28,14 @@ const PasswordGenerator = () => {
 	return (
 		<div className='flex h-full'>
 			<div className='m-auto w-5/6 md:w-1/2 lg:w-1/3'>
-				<h1 className='w-3/4 mb-10 text-gray-100 text-center text-3xl'>Generador de contraseñas</h1>
-				{copied &&
-				<p className='w-3/4 mb-10 p-2 text-purple-800 font-bold text-center bg-purple-300 border-l-4 border-l-purple-700 rounded-r'>
-					¡Contraseña copiada!
-				</p>
-				}
+				<h1 className='w-3/4 mb-5 text-gray-100 text-center text-3xl'>Generador de contraseñas</h1>
+				<div className='h-10 mb-5'>
+					{copied &&
+					<p className='w-3/4 p-2 text-purple-800 font-bold text-center bg-purple-300 border-l-4 border-l-purple-700 rounded-r'>
+						¡Contraseña copiada!
+					</p>
+					}
+				</div>
 				<div className='w-full h-10 mb-5'>
 					<input type='text' name='password' id='password' className='h-full w-3/4 p-4 rounded text-center bg-gray-100' readOnly value={password} />
 					<button className='ml-5 text-gray-100 underline' onClick={copyPassword}>Copiar</button>
