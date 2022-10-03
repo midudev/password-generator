dab222fde4bb0029f990c5c22ccc9174240b575f
Alexis Guzman <alexis12989x@gmail.com>
2022-10-03 17:17:25 -0500

    feat(components): setup input icon button component

--- src/components/codingcodax/InputIconButton.jsx
+++ src/components/codingcodax/InputIconButton.jsx
@@ -1,8 +1,12 @@
-const InputIconButton = () => {
+const InputIconButton = ({ children, onClick, isDisabled = false }) => {
 	return (
-		<div>
-			<p>Input Icon Button Component</p>
-		</div>
+		<button
+			className={`p-1 w-8 h-8 rounded-md bg-zinc-700 border border-zinc-600 duration-200 hover:bg-zinc-600 active:scale-90 ${isDisabled ? 'disabled:opacity-50 disabled:pointer-events-none' : ''}`}
+			onClick={onClick}
+			disabled={isDisabled}
+		>
+			{children}
+		</button>
 	)
 }
 
