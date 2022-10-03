d196ae8cfb4c424f9778726a9edd49861c18d94b
Alexis Guzman <alexis12989x@gmail.com>
2022-10-03 17:16:48 -0500

    feat(components): remove password from generate password button

--- src/components/codingcodax/GeneratePassowordButton.jsx
+++ src/components/codingcodax/GeneratePassowordButton.jsx
@@ -6,7 +6,7 @@ const GeneratePasswordButton = ({ handleChange }) => {
 			className='py-4 px-6 w-full flex items-center justify-center gap-4 text-indigo-100 bg-indigo-600 rounded-xl duration-200 hover:bg-indigo-700 active:scale-90'
 			onClick={handleChange}
 		>
-			Generate Password <RefreshIcon className='stroke-indigo-100 w-5 h-5' />
+			Generate <RefreshIcon className='stroke-indigo-100 w-5 h-5' />
 		</button>
 	)
 }
