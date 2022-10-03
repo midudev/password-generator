ece110fa03af29d3dbe132897c82f0a82d2de3a7
DiegoCadavid <dcmgames50@gmail.com>
2022-10-03 18:30:54 -0500

    remove before responsive desing

--- src/components/diegocadavid/CliTerminal.jsx
+++ src/components/diegocadavid/CliTerminal.jsx
@@ -4,7 +4,7 @@ import CliForm from './CliForm'
 
 const CliTerminal = ({addCommand, handleCloseWindow, handleOpenTerminal}) => {
   return (
-	<div className='block sm:w-40  md:top-2 md:right-2 lg:top-32 lg:right-24 md:w-52 lg:w-64 md:absolute rounded-lg overflow-hidden ring-2 ring-slate-400 animation-open'>
+	<div className='block top-32 right-24 w-64 absolute rounded-lg overflow-hidden ring-2 ring-slate-400 animation-open'>
 		<TitleWindow handleCloseWindow={handleCloseWindow} name='Password CLI' dark={false}/>
 		<CliForm handleOpenTerminal={handleOpenTerminal} addCommand={addCommand}/>
 	</div>
