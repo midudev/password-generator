31cb7c5c51bbf99d53856f57d71dc735e35e95a9
Juan Pablo Barrera <juanpablo.is@outlook.com>
2022-10-03 13:19:31 -0500

    Create hook to add timer on alert

---
+++ src/components/juanpablo-is/hooks/useTimerAlert.jsx
@@ -0,0 +1,98 @@
+import { memo, useState } from 'react'
+
+const useTimerAlert = () => {
+	const [alerts, setAlerts] = useState([])
+	const [refreshAlerts, setRefreshAlerts] = useState(true)
+
+	const Alerts = memo(() => {
+		return (
+			<div className='mt-3 absolute top-0 left-0 right-0 flex flex-col items-center md:items-start'>
+				{refreshAlerts &&
+					alerts.map((alert) => (
+						<div
+							key={alert.id}
+							id='toast-default'
+							className='m-1 sm:m-2 flex items-center p-4 w-72 sm:w-full max-w-xs text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800'
+							role='alert'
+						>
+							<div className='inline-flex flex-shrink-0 justify-center items-center w-8 h-8 text-blue-500 bg-blue-100 rounded-lg dark:bg-blue-800 dark:text-blue-200'>
+								<svg
+									aria-hidden='true'
+									className='w-5 h-5'
+									fill='currentColor'
+									viewBox='0 0 20 20'
+									xmlns='http://www.w3.org/2000/svg'
+								>
+									<path
+										fillRule='evenodd'
+										d='M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z'
+										clipRule='evenodd'
+									></path>
+								</svg>
+							</div>
+							<div className='ml-3 text-sm font-normal text-white'>{alert.children}</div>
+						</div>
+					))}
+			</div>
+		)
+	}, [alerts])
+
+	const generateAlert = (children, delay, type) => {
+		setRefreshAlerts(false)
+
+		class Timer {
+			constructor(children, delay, type) {
+				this.timerId = null
+				let start
+				const remaining = delay
+
+				this.pause = function () {
+					window.clearTimeout(this.timerId)
+
+					this.timerId = null
+					remaining -= Date.now() - start
+				}
+
+				this.resume = function () {
+					if (this.timerId) {
+						return
+					}
+
+					start = Date.now()
+
+					this.timerId = window.setTimeout(
+						() => setAlerts((prev) => prev.filter((alert) => alert.id !== this.timerId) || []),
+						remaining
+					)
+
+					setAlerts((prev) => {
+						const findSameType = prev.findIndex((alert) => alert.type === type)
+						const newAlert = { id: this.timerId, children, type }
+
+						if (findSameType >= 0) {
+							const newPrev = prev
+							prev[findSameType] = newAlert
+
+							return newPrev
+						}
+
+						return [...prev, newAlert]
+					})
+
+					setRefreshAlerts(true)
+				}
+
+				this.resume()
+			}
+		}
+
+		new Timer(children, delay, type)
+	}
+
+	return {
+		Alerts,
+		generateAlert
+	}
+}
+
+export default useTimerAlert
--- src/components/juanpablo-is/index.jsx
+++ src/components/juanpablo-is/index.jsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from 'react'
-import useGeneratePassword from './hooks/useGeneratePassword'
-import useLocalStorage from './hooks/useLocalStorage'
-import PasswordContext from './context/PasswordContext'
+// Components
+import InputPassword from './components/InputPassword'
 import InputRange from './components/inputRange'
 import CheckboxOptions from './components/CheckboxOptions'
-import InputPassword from './components/InputPassword'
+
+// Hooks
+import useGeneratePassword from './hooks/useGeneratePassword'
+import useLocalStorage from './hooks/useLocalStorage'
+import useTimerAlert from './hooks/useTimerAlert'
+
+// Contexts
+import PasswordContext from './context/PasswordContext'
 
 const App = () => {
-	const [alert, setAlert] = useState('')
+	const { generateAlert, Alerts } = useTimerAlert()
 	const [passwordLength, setPasswordLength] = useLocalStorage('length', '25')
 	const [automaticGenerator, setAutomaticGenerator] = useLocalStorage('automaticGenerator', true)
 	const [optionsCharacter, setOptionsCharacter] = useLocalStorage('optionsCharacter', {
@@ -21,24 +27,38 @@ const App = () => {
 		optionsCharacter
 	})
 
+	const handlerCopyPassword = () => {
+		navigator.clipboard.writeText(document.querySelector('#password-generated').value)
+		generateAlert('Password copied to clipboard!', 3000, 'copy')
+	}
+
 	const handlerNewPassword = (delay = false) => {
-		setAlert('')
+		const alertPassword = () =>
+			generateAlert(
+				<>
+					New password generated.
+					<span
+						className='text-blue-500 font-bold cursor-pointer pl-2'
+						onClick={handlerCopyPassword}
+					>
+						Copy
+					</span>
+				</>,
+				5000,
+				'password'
+			)
 
 		if (delay) {
 			const delayDebounceFn = setTimeout(() => {
 				generateNewPassword()
-				setAlert('New password generated.')
+				alertPassword()
 			}, 300)
 
 			return () => clearTimeout(delayDebounceFn)
 		}
 
 		generateNewPassword()
-		setAlert('New password generated.')
-	}
-
-	const handlerCopyPassword = () => {
-		navigator.clipboard.writeText(password)
+		alertPassword()
 	}
 
 	const handlerSaveConfiguration = () => {
@@ -50,6 +70,7 @@ const App = () => {
 				optionsCharacter
 			})
 		)
+		generateAlert('Options saved!', 3000, 'options')
 	}
 
 	useEffect(() => {
@@ -57,16 +78,6 @@ const App = () => {
 			return handlerNewPassword(true)
 		}
 	}, [passwordLength, optionsCharacter])
-
-	useEffect(() => {
-		if (password) {
-			const delayDebounceFn = setTimeout(() => {
-				setAlert('')
-			}, 3000)
-
-			return () => clearTimeout(delayDebounceFn)
-		}
-	}, [password])
 
 	return (
 		<PasswordContext.Provider
@@ -78,11 +89,12 @@ const App = () => {
 				passwordLength,
 				setPasswordLength,
 				automaticGenerator,
-				setAutomaticGenerator
+				setAutomaticGenerator,
+				generateAlert
 			}}
 		>
 			<div className='h-screen flex flex-col items-center justify-center text-center w-10/12 m-auto'>
-				<h1 className='mb-6 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
+				<h1 className='mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
 					Password Generator
 				</h1>
 
@@ -90,15 +102,14 @@ const App = () => {
 					Create strong and secure passwords
 				</p>
 
-				<div className='flex flex-col w-full max-w-xl my-4'>
+				<div className='flex flex-col w-full max-w-xl my-2'>
 					<InputPassword />
-					<hr className='my-4' />
+					<hr className='my-2 sm:my-4' />
 
 					<InputRange />
-					<hr className='my-4' />
-
 					<CheckboxOptions />
-					<hr className='my-4' />
+
+					<hr className='my-2 sm:my-4' />
 
 					<button
 						onClick={handlerNewPassword}
@@ -153,40 +164,7 @@ const App = () => {
 					</div>
 				</div>
 
-				{alert && (
-					<div className='mt-3 absolute top-0 left-0 right-0 flex flex-col items-center'>
-						<div
-							id='toast-default'
-							className='m-1 sm:m-2 flex items-center p-4 w-72 sm:w-full max-w-xs text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800'
-							role='alert'
-						>
-							<div className='inline-flex flex-shrink-0 justify-center items-center w-8 h-8 text-blue-500 bg-blue-100 rounded-lg dark:bg-blue-800 dark:text-blue-200'>
-								<svg
-									aria-hidden='true'
-									className='w-5 h-5'
-									fill='currentColor'
-									viewBox='0 0 20 20'
-									xmlns='http://www.w3.org/2000/svg'
-								>
-									<path
-										fillRule='evenodd'
-										d='M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z'
-										clipRule='evenodd'
-									></path>
-								</svg>
-							</div>
-							<div className='ml-3 text-sm font-normal'>
-								{alert}
-								<span
-									className='text-blue-500 font-bold cursor-pointer pl-2'
-									onClick={handlerCopyPassword}
-								>
-									Copy
-								</span>
-							</div>
-						</div>
-					</div>
-				)}
+				<Alerts />
 			</div>
 		</PasswordContext.Provider>
 	)
