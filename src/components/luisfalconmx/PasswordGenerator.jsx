8f4ee1a42fddc624939b962bd2f13e42cc87865c
Luis Falcon <luisfalconmx@gmail.com>
2022-10-02 22:24:15 -0500

    feat: control and clipboard components created and implemented

---
+++ src/components/luisfalconmx/PasswordGenerator.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Clipboard from './Clipboard'
+import Controls from './Controls'
+
+const PasswordGenerator = () => {
+	return (
+		<>
+			<Clipboard />
+			<Controls />
+		</>
+	)
+}
+
+export default PasswordGenerator
