413ef4efce12003065cfd2cec7c4e59af12bc817
Alexis Guzman <alexis12989x@gmail.com>
2022-10-03 14:04:27 -0500

    feat(components): setup input slider component

--- src/components/codingcodax/InputSlider.jsx
+++ src/components/codingcodax/InputSlider.jsx
@@ -1,7 +1,21 @@
-const InputSlider = () => {
+const InputSlider = ({ length, handleChange, min, max }) => {
 	return (
-		<div>
-			<p>Input Slider Component</p>
+		<div className='grid gap-4'>
+			<div className='flex justify-between'>
+				<label htmlFor='length' className='text-zinc-400'>
+					Length:
+				</label>
+				<span className='text-indigo-300'>{length}</span>
+			</div>
+			<input
+				className='appearance-none rounded-md bg-zinc-800 h-2 focus:outline-dashed focus:outline-indigo-300'
+				type='range'
+				id='length'
+				min={min}
+				max={max}
+				value={length}
+				onChange={handleChange}
+			/>
 		</div>
 	)
 }
