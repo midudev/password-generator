b3eb935533d16a689f0a92533e37740cfa533cc2
Josel95 <jose.lopezh95@gmail.com>
2022-10-04 14:23:54 -0300

    Efecto de cargando contraseña

---
+++ src/components/Josel95/Password.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+
+interface PasswordProps {
+	value: string
+	blurred?: boolean
+}
+
+export const Password: FC<PasswordProps> = ({
+	value,
+	blurred = false
+}) => {
+	const classes = [
+		'josel95-password-generator-password',
+		blurred ? 'josel95-blurred' : ''
+	]
+	return <input className={classes.join(' ')} type='text' value={value} />
+}
--- src/components/Josel95/PasswordGenerator.tsx
+++ src/components/Josel95/PasswordGenerator.tsx
@@ -1,14 +1,24 @@
 import { ChangeEvent, useState } from 'react'
 
 import { generatePassword } from './utils/generatePassword'
+import { Password } from './Password'
 
 export const PasswordGenerator = () => {
 	const [passwordGenerated, setPasswordGenerated] = useState('')
 	const [passwordLength, setPasswordLength] = useState(10)
+	const [loading, setLoading] = useState(false)
 
 	const handlePasswordGenerate = () => {
-		const password = generatePassword(passwordLength)
-		setPasswordGenerated(password)
+		setLoading(true)
+		const interval = setInterval(() => {
+			const password = generatePassword(passwordLength)
+			setPasswordGenerated(password)
+		}, 50)
+
+		setTimeout(() => {
+			setLoading(false)
+			clearInterval(interval)
+		}, 1000)
 	}
 
 	const handleLengthChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -19,7 +29,7 @@ export const PasswordGenerator = () => {
 	return (
 		<div className='josel95-password-generator-container'>
 			<h1 className='josel95-password-generator-title'>Password generator</h1>
-			<input className='josel95-password-generator-password' type='text' value={passwordGenerated} />
+			<Password value={passwordGenerated} blurred={loading}/>
 			<input className='josel95-password-generator-length' type='range' min={5} max={64} defaultValue={10} onChange={handleLengthChange}/>
 			<button className='josel95-password-generator-button' onClick={handlePasswordGenerate}>Generate</button>
 		</div>
