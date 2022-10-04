5387f4b066d3ca6a7891d6e457206fe5b2fbf203
Johan Santana <johancarlosst@gmail.com>
2022-10-04 10:31:36 -0400

    feat: making the solution responsive

--- src/components/johansantana/PasswordGenerator.vue
+++ src/components/johansantana/PasswordGenerator.vue
@@ -60,17 +60,19 @@ const handleCopy = () => {
 		<!-- Generator -->
 		<div class="py-10 flex flex-col">
 			<h1
-				id="johansantana__h1"
+				id="johansantana-h1"
 				class="text-5xl bg-clip-text text-transparent bg-gradient-to-r from-sky-400 to-pink-500 mb-4"
 			>
 				Password Generator
 			</h1>
-			<p class="text-gray-200/50 mb-2 mx-auto before:content-['-'] after:content-['-'] after:ml-2">
+			<p
+				class="text-sm sm:text-base text-gray-200/50 mb-2 mx-auto sm:before:content-['-'] sm:after:content-['-'] sm:after:ml-2"
+			>
 				Select your <span class="text-gray-200/80">password length</span>
 			</p>
 			<div class="mb-6 flex gap-6 items-center">
 				<input
-					id="johansantana__range"
+					id="johansantana-range"
 					type="range"
 					:min="MIN_LENGTH"
 					:max="MAX_LENGTH"
@@ -80,6 +82,7 @@ const handleCopy = () => {
 				<span class="text-white font-bold text-3xl">{{ passwordLength }}</span>
 			</div>
 			<button
+				id="johansantana-cta-button"
 				class="px-6 bg-blue-700 rounded-full py-3 text-white text-lg mb-6 hover:scale-105 hover:bg-blue-600 transition outline-8 outline-offset-4"
 				@click="handleGenerateNewPassword(passwordLengthNumber)"
 			>
@@ -91,9 +94,10 @@ const handleCopy = () => {
 					:value="password"
 					placeholder="here goes your password..."
 					readonly
-					class="bg-transparent border-2 border-white text-white placeholder-gray-400 rounded-lg focus:outline-none block w-full p-3"
+					class="bg-transparent border-2 text-sm sm:text-base border-white text-white placeholder-gray-400 rounded-lg focus:outline-none block w-full p-3"
 				/>
 				<button
+					id="johansantana-copy-button"
 					class="bg-transparent relative border-2 border-white p-2 px-3 rounded-lg text-white hover:bg-white hover:scale-110 hover:text-gray-700 focus:outline-none transition"
 					@click="handleCopy"
 				>
@@ -111,12 +115,12 @@ const handleCopy = () => {
 			class="py-10 text-white flex flex-col items-center gap-6 border-t-8 border-dashed border-gray-400/20"
 		>
 			<div class="w-fit">
-				<img src="/logo.svg" alt="HacktoberFest Logo" class="w-60 opacity-70" />
+				<img src="/logo.svg" alt="HacktoberFest Logo" class="w-44 sm:w-52 md:w-60 opacity-70" />
 			</div>
 			<div class="w-fit">
 				<a
 					href="https://github.com/johansantana"
-					class="flex gap-3 p-2 hover:bg-slate-50/10 rounded-full transition"
+					class="flex text-sm sm:text-base gap-3 p-2 hover:bg-slate-50/10 rounded-full transition"
 					target="_blank"
 					rel="noopener"
 				>
@@ -129,11 +133,11 @@ const handleCopy = () => {
 </template>
 
 <style scoped>
-#johansantana__h1 {
+#johansantana-h1 {
 	font-family: 'Inter', sans-serif;
 }
 
-#johansantana__range::-webkit-slider-thumb {
+#johansantana-range::-webkit-slider-thumb {
 	-webkit-appearance: none;
 	appearance: none;
 	width: 24px;
@@ -144,8 +148,13 @@ const handleCopy = () => {
 	transition: background-color 0.2s ease;
 }
 
-#johansantana__range:hover::-webkit-slider-thumb {
+#johansantana-range:hover::-webkit-slider-thumb {
 	border-radius: 999%;
 	background-color: rgb(200, 200, 200);
+}
+
+#johansantana-cta-button,
+#johansantana-copy-button {
+	-webkit-tap-highlight-color: transparent;
 }
 </style>
