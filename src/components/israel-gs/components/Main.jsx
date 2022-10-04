1a85a11d457cf5678f2d5aa021f8213869fe5c70
José Israel Gutierrez Salazar <israelgs83@gmail.com>
2022-10-04 10:54:14 -0500

    feat: adding alert message
    Adding alert message when user copy password to clipboard

--- src/components/israel-gs/components/Main.jsx
+++ src/components/israel-gs/components/Main.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 import PasswordFilter from './PasswordFilter.jsx'
 import PasswordResult from './PasswordResult.jsx'
+import Alert from './Alert.jsx'
 import passwordGenerator from '@components/israel-gs/helpers/passwordGenerator.ts'
 
 export default function Main() {
 	const [password, setPassword] = useState('...')
 	const [filters, setFilters] = useState([])
+
+	const [showAlert, setShowAlert] = useState(false)
+	const [alertMessage, setAlertMessage] = useState('')
+	const [alertType, setAlertType] = useState('success')
 
 	const handleFilterChange = (filters) => {
 		setFilters(filters)
@@ -17,11 +22,22 @@ export default function Main() {
 	}
 
 	const handleCopy = async () => {
-		await navigator.clipboard.writeText(password)
+		navigator.clipboard
+			.writeText(password)
+			.then(() => {
+				setAlertMessage('Copiado al portapapeles')
+				setShowAlert(true)
+			})
+			.catch(() => {
+				setAlertType('error')
+				setAlertMessage('No se pudo copiar al portapapeles')
+				setShowAlert(true)
+			})
 	}
 
 	return (
 		<>
+			<Alert type={alertType} message={alertMessage} show={showAlert} setShow={setShowAlert} />
 			<PasswordResult onCopy={handleCopy} onRefresh={handleRefresh} password={password} />
 			<PasswordFilter onChange={handleFilterChange} />
 		</>
