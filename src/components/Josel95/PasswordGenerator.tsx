f6c360d09dc22499c84bb74e860fc87036cd437a
Josel95 <jose.lopezh95@gmail.com>
2022-10-03 21:05:54 -0300

    Agrega input range para cambiar largo de password

--- src/components/Josel95/PasswordGenerator.tsx
+++ src/components/Josel95/PasswordGenerator.tsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 
 import { generatePassword } from './utils/generatePassword'
 
 export const PasswordGenerator = () => {
 	const [passwordGenerated, setPasswordGenerated] = useState('')
+	const [passwordLength, setPasswordLength] = useState(10)
 
 	const handlePasswordGenerate = () => {
-		const password = generatePassword(10)
+		const password = generatePassword(passwordLength)
 		setPasswordGenerated(password)
+	}
+
+	const handleLengthChange = (event: ChangeEvent<HTMLInputElement>) => {
+		const rangeValue = parseInt(event.target.value)
+		setPasswordLength(rangeValue)
 	}
 
 	return (
 		<div className='josel95-password-generator-container'>
 			<h1>Password generator</h1>
 			<span>{passwordGenerated}</span>
+			<input type='range' min={5} max={64} defaultValue={10} onChange={handleLengthChange}/>
 			<button onClick={handlePasswordGenerate}>Generate</button>
 		</div>
 	)
