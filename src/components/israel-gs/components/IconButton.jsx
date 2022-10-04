02a0c7b4a6197f9422bd0e27c6188495c4dc61d8
José Israel Gutierrez Salazar <israelgs83@gmail.com>
2022-10-04 00:20:29 -0500

    feat: firts layout design

---
+++ src/components/israel-gs/components/IconButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function IconButton({ handleClick, src }) {
+	console.log('IconButton', { handleClick, src })
+	return (
+		<button
+			onClick={() => {
+				console.log('IconButton onClick')
+				handleClick()
+			}}
+			className='flex justify-center items-center p-2 rounded-full hover:bg-gray-500/20 transition-colors w-[50px] h-[50px]'
+		>
+			<img src={src} alt='' srcSet='' className='' />
+		</button>
+	)
+}
