f19241464f6af8e0bd753f44ce87a6893042c914
Carlos Ivanchuk <buhodev@proton.me>
2022-10-04 12:59:18 -0400

    feat: copy password on click

--- src/components/buhodev/PasswordGenerator.svelte
+++ src/components/buhodev/PasswordGenerator.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   	import { onMount } from "svelte"
 	import { generate_password, type DefaultOptions } from "./generate_password"
+	import {copy_to_clipboard} from "./copy_to_clipboard"
 	
 	let password = ""
 	let isCopied = false;
@@ -12,7 +13,7 @@
 		symbols: false
 	}
 	
-	function copy() {
+	function handleSuccessfulCopy() {
 		isCopied = true
 		setTimeout(() => isCopied = false, 1000)
 	}
@@ -30,7 +31,7 @@
 			{password}
 		</span>
 
-		<button on:click={copy} title="Copy password" class="rounded-md border-2 border-slate-700 hover:border-slate-600 flex items-center justify-center py-1 px-2">
+		<button use:copy_to_clipboard={{text: password}} on:copied={handleSuccessfulCopy} on:error={() => alert("error")} title="Copy password" class="rounded-md border-2 border-slate-700 hover:border-slate-600 flex items-center justify-center py-1 px-2">
 			{#if !isCopied}
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 					<path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
