d1d4435e572c27537481524aeb4fb103a17aff90
David Viuche <jviucheroja@uniminuto.edu.co>
2022-10-04 19:46:42 -0500

    add page base

---
+++ src/components/David-Viuche/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+const App = () => {
+	return (
+		<div id='app-dv' className='center-dv'>
+			<section id='gen-dv' className='center-dv'>
+				<form id='form-dv' className='center-dv'>
+					<div id='pass-cont-dv'>
+						<span id='pass-dv'>agagagajahsbfkdmencj</span>
+						<img src='/public/David-Viuche/copy.svg' alt='copiar contraseña' />
+					</div>
+					<input type='range' />
+					<button>Generar</button>
+				</form>
+			</section>
+		</div>
+	)
+}
+
+export default App
