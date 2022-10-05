1c3aeb55265e7055856a646c0a0a906777f794fa
Carlos Garcia <carlossgarcia@outlook.com>
2022-10-05 00:21:09 -0600

    create csharls password generator

---
+++ src/components/csharls/Generator/index.jsx
@@ -0,0 +1,17 @@
+import PasswordDisplay from '../PasswordDisplay'
+import Controls from '../Controls'
+import { GeneratorContextProvider } from '../context/generatorContext'
+import './generator.css'
+
+export default function Generator() {
+	return (
+		<>
+			<div className='border rounded-md border-yellow-400 p-10 shadow-md shadow-lg card shadow-indigo-500/50 text-slate-200'>
+				<GeneratorContextProvider>
+					<Controls />
+					<PasswordDisplay />
+				</GeneratorContextProvider>
+			</div>
+		</>
+	)
+}
