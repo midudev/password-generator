9f59d6ea91f806a662a01175e18c1b03caf8894c
Christian Lara <chris.larasaa@gmail.com>
2022-10-05 21:21:47 -0300

    fix center css

--- src/components/chrislarasaa/PasswordGenerator.jsx
+++ src/components/chrislarasaa/PasswordGenerator.jsx
@@ -1,4 +1,4 @@
-import { useState} from 'react'
+import { useState } from 'react'
 import Container from '@components/chrislarasaa/Container'
 import Slider from '@components/chrislarasaa/Slider'
 import Button from '@components/chrislarasaa/Button'
@@ -10,7 +10,7 @@ const PasswordGenerator = () => {
 
 	const [sliderValue, setSliderValue] = useState(6)
 	const [password, setPassword] = useState('')
-	
+
 	const handleGeneratePassword = () => {
 		const numbers = "0123456789";
 		const lowerCase = "abcdefghijklmnopqrstuvwxyz"
@@ -18,27 +18,30 @@ const PasswordGenerator = () => {
 		const specialChar = ""
 		var pwdLen = sliderValue;
 
-		const randomPassword = Array(pwdLen).fill(numbers + lowerCase + upperCase + specialChar).map( x => x[Math.floor(Math.random() * x.length)]).join('');
+		const randomPassword = Array(pwdLen).fill(numbers + lowerCase + upperCase + specialChar).map(x => x[Math.floor(Math.random() * x.length)]).join('');
 		setPassword(randomPassword)
 	}
-	
-	return (
-		<Container title="Password Generator">
-			<PasswordInput 
-				placeholder="Password generated"
-				password={password}
-			/>
 
-			<h1 className='char-lenght'>Password Length: {sliderValue}</h1>
-			<Slider
-				sliderValue={sliderValue}
-				setSliderValue={setSliderValue}
-			/>
-			<Button 
-				handleGeneratePassword={handleGeneratePassword}
-				buttonText="Generate Random Password"
-			/>
-		</Container>
+	return (
+		<section className='section-center'>
+			<Container title="Password Generator">
+				<PasswordInput
+					placeholder="Password generated"
+					password={password}
+				/>
+
+				<h1 className='char-lenght'>Password Length: {sliderValue}</h1>
+				<Slider
+					sliderValue={sliderValue}
+					setSliderValue={setSliderValue}
+				/>
+				<Button
+					handleGeneratePassword={handleGeneratePassword}
+					buttonText="Generate Random Password"
+				/>
+			</Container>
+		</section>
+
 	)
 }
 
