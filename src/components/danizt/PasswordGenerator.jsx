34c4e300d740d318471da29321e931dfe4ef14f3
Dani <danizt96@gmail.com>
2022-10-06 22:20:14 +0200

    add copy button

--- src/components/danizt/PasswordGenerator.jsx
+++ src/components/danizt/PasswordGenerator.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const NUMBERS = '0123456789'
 const LOWER_CASE = 'abcdefghijklmnopqrstuvwxyz'
 const UPPER_CASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const SPECIAL = ' !#$%&()*+,-./:;<=>?@[]^_`{|}~'
+const SPECIAL = '!#$%&()*+,-./:;<=>?@[]^_`{|}~'
 
 function getRandomPassword(charactersAvaliable, length) {
 	let randomPassword = ''
@@ -18,11 +18,7 @@ function getRandomPassword(charactersAvaliable, length) {
 const PasswordGenerator = () => {
 	const [password, setPassword] = useState('')
 
-	function handleSubmitForm(e) {
-		e.preventDefault()
-		// TODO: get form data
-		// const formData = new FormData(e.target)
-
+	function handleGeneratePassword() {
 		let charactersAvaliable = ''
 		// TODO: if from checkbox
 		charactersAvaliable += NUMBERS
@@ -36,25 +32,37 @@ const PasswordGenerator = () => {
 		setPassword(getRandomPassword(charactersAvaliable, length))
 	}
 
+	function handleCopy() {
+		navigator.clipboard.writeText(password).then(() => {
+			// TODO: shsow success copy to clipboard message
+		})
+	}
+
 	return (
 		<>
 			<h2 className='flex justify-center my-4 mt-0 font-medium leading-tight text-4xl text-blue-300'>
 				Password Generator
 			</h2>
-			<div className='border border-l-white rounded-lg p-8'>
-				<form className='flex flex-col gap-y-4' onSubmit={handleSubmitForm}>
-					<label
-						id='password-generated'
-						type='text'
-						className='rounded-lg outline-0 py-2 px-2 text-gray-800 bg-white text-center w-96'
-					>
-						{password || 'Password'}
-					</label>
+			<div className='border border-l-white rounded-lg p-8 flex flex-col gap-y-4'>
+				<label
+					id='password-generated'
+					type='text'
+					className='rounded-lg outline-0 py-2 px-2 text-gray-800 bg-white text-center w-96'
+				>
+					{password || '👇 Generate a strong password 👇'}
+				</label>
 
-					<button type='submit' className='border border-l-white rounded-lg py-2 px-2'>
+				<div className='flex flex-row gap-4'>
+					<button
+						onClick={handleGeneratePassword}
+						className='border border-l-white rounded-lg py-2 px-2 w-1/2'
+					>
 						Generate password
 					</button>
-				</form>
+					<button onClick={handleCopy} className='border border-l-white rounded-lg py-2 px-2 w-1/2'>
+						Copy
+					</button>
+				</div>
 			</div>
 		</>
 	)
