faba874633a872fd77df3e64a2724c250eaa7ae0
Dani <danizt96@gmail.com>
2022-10-07 17:07:55 +0200

    Copy button style

--- src/components/danizt/PasswordGenerator.jsx
+++ src/components/danizt/PasswordGenerator.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { getRandomPassword, copyPassword } from './utils/handlePassword'
+import CopySvg from './icons/copySvg'
 
 const NUMBERS = '0123456789'
 const LOWER_CASE = 'abcdefghijklmnopqrstuvwxyz'
@@ -35,13 +36,22 @@ const PasswordGenerator = () => {
 			<h2 className='flex justify-center my-4 mt-0 font-medium leading-tight text-4xl text-blue-300'>
 				Password Generator
 			</h2>
-			<div className='border border-l-white rounded-lg p-8 flex flex-col gap-y-4'>
-				<label
-					type='text'
-					className='rounded-lg outline-0 py-2 px-2 text-gray-800 bg-white text-center w-96'
-				>
-					{password}
-				</label>
+			<div className='border border-white rounded-lg p-8 flex flex-col gap-y-4'>
+				<div className='flex gap-2'>
+					<label
+						type='text'
+						className='rounded-lg outline-0 py-2 px-2 text-gray-800 bg-white text-center w-96'
+					>
+						{password}
+					</label>
+					<button
+						onClick={handleCopy}
+						disabled={!password}
+						className='border border-green-500 rounded-lg py-2 px-2'
+					>
+						<CopySvg />
+					</button>
+				</div>
 				<label className='flex justify-center gap-2'>
 					<span>Password length:</span>
 					<span className='text-blue-300 font-bold'>{passwordLength}</span>
@@ -57,21 +67,12 @@ const PasswordGenerator = () => {
 					}}
 				/>
 
-				<div className='flex flex-row gap-4'>
-					<button
-						onClick={handleGeneratePassword}
-						className='border border-l-white rounded-lg py-2 px-2 w-2/3'
-					>
-						Generate password
-					</button>
-					<button
-						onClick={handleCopy}
-						disabled={!password}
-						className='border border-l-white rounded-lg py-2 px-2 w-1/3'
-					>
-						Copy
-					</button>
-				</div>
+				<button
+					onClick={handleGeneratePassword}
+					className='border border-white rounded-lg py-2 px-2'
+				>
+					Generate password
+				</button>
 			</div>
 		</>
 	)
