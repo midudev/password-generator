f176b15f5ee8ef50397278ac2bf2ecf4dc24d140
Johan Santana <johancarlosst@gmail.com>
2022-10-06 22:44:49 -0400

    feat: added on render animation

--- src/components/johansantana/PasswordGenerator.vue
+++ src/components/johansantana/PasswordGenerator.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { ref, computed } from 'vue'
-import { animate, newPassword } from './utils.js'
+import { animate, newPassword } from './utils'
 import TooltipMessage from './TooltipMessage.vue'
 import WarningMessage from './WarningMessage.vue'
+import Appear from './Appear.vue'
 import GithubIcon from './icons/GithubIcon.vue'
 import CopyIcon from './icons/CopyIcon.vue'
 import CheckIcon from './icons/CheckIcon.vue'
@@ -51,83 +52,97 @@ const handleCopy = (text) => {
 
 <template>
 	<div class="flex flex-col gap-10 relative">
-		<Transition name="slide">
+		<Transition name="slidedown">
 			<WarningMessage v-if="warningMessage" :message="warningMessage" />
 		</Transition>
+
 		<!-- Generator -->
 		<div class="py-10 flex flex-col">
-			<h1
-				id="johansantana-h1"
-				class="text-5xl bg-clip-text text-transparent bg-gradient-to-r from-sky-400 to-pink-500 mb-4"
-			>
-				Password Generator
-			</h1>
-			<p
-				class="text-sm sm:text-base text-gray-200/50 mb-2 mx-auto sm:before:content-['-'] sm:after:content-['-'] sm:after:ml-2"
-			>
-				Select your <span class="text-gray-200/80">password length</span>
-			</p>
-			<div class="mb-6 flex gap-6 items-center">
-				<input
-					id="johansantana-range"
-					type="range"
-					:min="MIN_LENGTH"
-					:max="MAX_LENGTH"
-					v-model="passwordLength"
-					class="w-full h-2 bg-gray-700 rounded-lg appearance-none cursor-pointer focus:outline-none"
-				/>
-				<span class="text-white font-bold text-3xl">{{ passwordLength }}</span>
-			</div>
-			<button
-				id="johansantana-cta-button"
-				class="px-6 bg-blue-700 rounded-full py-3 text-white text-lg mb-6 hover:scale-105 hover:bg-blue-600 transition outline-8 outline-offset-4"
-				@click="handleGenerateNewPassword(passwordLengthNumber)"
-			>
-				Generate!
-			</button>
-			<div class="flex gap-3">
-				<input
-					type="text"
-					:value="animatedPassword"
-					placeholder="here goes your password..."
-					readonly
-					class="bg-transparent border-2 text-sm sm:text-base border-white text-white placeholder-gray-400 rounded-lg focus:outline-none block w-full p-3"
-				/>
-				<button
-					id="johansantana-copy-button"
-					class="bg-transparent relative border-2 border-white p-2 px-3 rounded-lg text-white hover:bg-white hover:scale-110 hover:text-gray-700 focus:outline-none transition"
-					@click="handleCopy(password)"
+			<Appear :delay="100">
+				<h1
+					id="johansantana-h1"
+					class="text-4xl sm:text-5xl font-sans font-bold bg-clip-text text-transparent bg-gradient-to-r from-sky-400 to-pink-500 mb-4"
 				>
-					<CopyIcon />
+					Password Generator
+				</h1>
+			</Appear>
 
-					<Transition name="fade">
-						<TooltipMessage v-if="showTooltipMessage" message="Copied!">
-							<CheckIcon class="text-green-400" />
-						</TooltipMessage>
-					</Transition>
+			<Appear :delay="200">
+				<p
+					class="text-sm sm:text-base text-gray-200/50 mb-2 w-fit mx-auto sm:before:content-['-'] sm:after:content-['-'] sm:after:ml-2"
+				>
+					Select your <span class="text-gray-200/80">password length</span>
+				</p>
+				<div class="mb-6 flex gap-6 items-center">
+					<input
+						id="johansantana-range"
+						type="range"
+						:min="MIN_LENGTH"
+						:max="MAX_LENGTH"
+						v-model="passwordLength"
+						class="w-full h-2 bg-gray-700 rounded-lg appearance-none cursor-pointer focus:outline-none"
+					/>
+					<span class="text-white font-bold text-2xl md:text-3xl">{{ passwordLength }}</span>
+				</div>
+			</Appear>
+
+			<Appear :delay="300">
+				<button
+					id="johansantana-cta-button"
+					class="px-6 text-sm sm:text-lg w-full bg-blue-700 rounded-full py-3 text-white mb-6 hover:scale-105 hover:bg-blue-600 transition outline-8 outline-offset-4"
+					@click="handleGenerateNewPassword(passwordLengthNumber)"
+				>
+					Generate!
 				</button>
-			</div>
+			</Appear>
+
+			<Appear :delay="400">
+				<div class="flex gap-3">
+					<input
+						type="text"
+						:value="animatedPassword"
+						placeholder="here goes your password..."
+						readonly
+						class="bg-transparent border-2 text-sm sm:text-base border-white text-white placeholder-gray-400 rounded-lg focus:outline-none block w-full p-3"
+					/>
+					<button
+						id="johansantana-copy-button"
+						class="bg-transparent relative border-2 border-white p-2 px-3 rounded-lg text-white hover:bg-white hover:scale-110 hover:text-gray-700 focus:outline-none transition"
+						@click="handleCopy(password)"
+					>
+						<CopyIcon />
+
+						<Transition name="fade">
+							<TooltipMessage v-if="showTooltipMessage" message="Copied!">
+								<CheckIcon class="text-green-400" />
+							</TooltipMessage>
+						</Transition>
+					</button>
+				</div>
+			</Appear>
 		</div>
 
 		<!-- Footer -->
-		<footer
-			class="py-10 text-white flex flex-col items-center gap-6 border-t-8 border-dashed border-gray-400/20"
-		>
-			<div class="w-fit">
-				<img src="/logo.svg" alt="HacktoberFest Logo" class="w-44 sm:w-52 md:w-60 opacity-70" />
-			</div>
-			<div class="w-fit">
-				<a
-					href="https://github.com/johansantana"
-					class="flex text-sm sm:text-base gap-3 p-2 hover:bg-slate-50/10 rounded-full transition"
-					target="_blank"
-					rel="noopener"
-				>
-					<GithubIcon />
-					@johansantana
-				</a>
-			</div>
-		</footer>
+		<Appear :delay="500">
+			<footer
+				class="py-10 text-white flex flex-col items-center gap-6 border-t-8 border-dashed border-gray-400/20"
+			>
+				<div class="w-fit">
+					<img src="/logo.svg" alt="HacktoberFest Logo" class="w-44 sm:w-52 md:w-60 opacity-70" />
+				</div>
+				<div class="w-fit">
+					<a
+						href="https://github.com/johansantana"
+						class="flex text-sm sm:text-base gap-3 p-2 hover:bg-slate-50/10 rounded-full transition"
+						target="_blank"
+						rel="noopener"
+					>
+						<GithubIcon />
+						@johansantana
+					</a>
+				</div>
+			</footer>
+		</Appear>
 	</div>
 </template>
 
