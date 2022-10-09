0ab866f491e50c6ea4c45cd5920b385d6dddbdac
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-09 10:53:50 -0400

    feat:add styles and change between layouts

--- src/components/cmglezpdev/Settings.tsx
+++ src/components/cmglezpdev/Settings.tsx
@@ -1,29 +1,29 @@
 
 export const Settings = () => {
 	return (
-		<div className='mt-8'>
-			<h2 className='text-2xl text-center mb-2'>Settings</h2>
+		<div className='mt-6'>
+			<h2 className='text-3xl text-center mb-2 text-white font-bold'>Settings</h2>
 
 			<div className='grid grid-cols-2 w-60'>
 				<div className='col-span-full text-center'>
-					<input type='checkbox' name='strong_password' className='mr-2' />
-					<label htmlFor='strong_password'>Strong Password</label>
+					<input type='checkbox' name='strong_password' className='mr-2 outline-none' />
+					<label htmlFor='strong_password' className='text-gray-400 text-xl'>Strong Password</label>
 				</div>
 				<div>
-					<input type='checkbox' name='letters' className='mr-2' />
-					<label htmlFor='letters'>Letters</label>
+					<input type='checkbox' name='letters' className='mr-2 outline-none' />
+					<label htmlFor='letters' className='text-gray-400 text-xl'>Letters</label>
 				</div>
 				<div>
-					<input type='checkbox' name='numbers' className='mr-2' />
-					<label htmlFor='numbers'>Numbers</label>
+					<input type='checkbox' name='numbers' className='mr-2 outline-none' />
+					<label htmlFor='numbers' className='text-gray-400 text-xl'>Numbers</label>
 				</div>
 				<div>
-					<input type='checkbox' name='div_in_groups' className='mr-2' />
-					<label htmlFor='div_in_groups'>Divide</label>
+					<input type='checkbox' name='div_in_groups' className='mr-2 outline-none' />
+					<label htmlFor='div_in_groups' className='text-gray-400 text-xl'>Divide</label>
 				</div>
 				<div>
-					<input type='checkbox' name='div_in-groups' className='mr-2' />
-					<label htmlFor='special_Characters'>#%!&@</label>
+					<input type='checkbox' name='div_in-groups' className='mr-2 outline-none' />
+					<label htmlFor='special_Characters' className='text-gray-400 text-xl'>#%!&@</label>
 				</div>
 			</div>
 		</div>
