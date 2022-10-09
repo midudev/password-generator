45a279d14f50dc62c576f756fbdb3fd6926de498
Javier Reyes <javier.reys@gmail.com>
2022-10-09 20:17:22 -0300

    feat: add password generation with symbols
    this also introduce the correct color for the corresponding type of char. Blue for numbers and red for symbols.

--- src/components/JaavierR/PasswordGenerator.jsx
+++ src/components/JaavierR/PasswordGenerator.jsx
@@ -9,6 +9,7 @@ function PasswordGenerator() {
 	const [password, setPassword] = useState(generatePassword({ length: passwordLen }))
 	const [copied, setCopied] = useState(false)
 	const [includeNumbers, setIncludeNumbers] = useState(false)
+	const [includeSymbols, setIncludeSymbols] = useState(false)
 
 	function copyClipboard() {
 		navigator.clipboard.writeText(password)
@@ -30,7 +31,7 @@ function PasswordGenerator() {
 		}
 
 		setPasswordLen(e.target.value)
-		setPassword(generatePassword({ length, includeNumbers }))
+		setPassword(generatePassword({ length, includeNumbers, includeSymbols }))
 	}
 
 	function checkLength(e) {
@@ -50,7 +51,23 @@ function PasswordGenerator() {
 	function handleSwitchChange() {
 		const numbers = !includeNumbers
 		setIncludeNumbers(numbers)
-		setPassword(generatePassword({ length: passwordLen, includeNumbers: numbers }))
+		setPassword(generatePassword({ length: passwordLen, includeNumbers: numbers, includeSymbols }))
+	}
+
+	function handleSwitchSymbol() {
+		const symbols = !includeSymbols
+		setIncludeSymbols(symbols)
+		setPassword(generatePassword({ length: passwordLen, includeNumbers, includeSymbols: symbols }))
+	}
+
+	function checkCharType(char) {
+		if (/^\d$/.test(char)) {
+			return 'text-blue-400'
+		} else if ([33, 42, 45, 46, 64, 95].includes(char.charCodeAt(0))) {
+			return 'text-red-400'
+		} else {
+			return ''
+		}
 	}
 
 	return (
@@ -71,7 +88,7 @@ function PasswordGenerator() {
 
 			<div className='mb-8 text-xl truncate w-full px-4 py-2 rounded-lg ring-1 ring-zinc-600/70'>
 				{[...password].map((char, idx) => (
-					<span key={idx} className={/^\d$/.test(char) ? 'text-blue-500' : ''}>
+					<span key={idx} className={checkCharType(char)}>
 						{char}
 					</span>
 				))}
@@ -110,14 +127,16 @@ function PasswordGenerator() {
 				<InputSwitch
 					label='Symbols'
 					value={includeNumbers}
-					onChange={() => setIncludeNumbers(!includeNumbers)}
+					onChange={handleSwitchSymbol}
 					className='py-4'
 				/>
 			</div>
 
 			<button
 				className='bg-pink-500 hover:bg-pink-600 mt-6 w-full py-2 rounded-lg uppercase text-sm font-semibold tracking-wide'
-				onClick={() => setPassword(generatePassword({ length: passwordLen, includeNumbers }))}
+				onClick={() =>
+					setPassword(generatePassword({ length: passwordLen, includeNumbers, includeSymbols }))
+				}
 			>
 				Generate
 			</button>
--- src/components/JaavierR/helper/passwordGenerator.js
+++ src/components/JaavierR/helper/passwordGenerator.js
@@ -10,7 +10,12 @@ function getNumber() {
 	return Math.floor(Math.random() * 10)
 }
 
-export function generatePassword({ length, includeNumbers }) {
+function getSymbol() {
+	const symbols = [33, 42, 45, 46, 64, 95]
+	return String.fromCharCode(symbols[Math.floor(Math.random() * symbols.length)])
+}
+
+export function generatePassword({ length, includeNumbers, includeSymbols }) {
 	let password = ''
 
 	for (let i = 0; i < parseInt(length); i++) {
@@ -23,6 +28,14 @@ export function generatePassword({ length, includeNumbers }) {
 				password += getLower()
 			} else if (x >= 0.66) {
 				password += getNumber()
+			}
+		} else if (includeSymbols) {
+			if (x < 0.33) {
+				password += getUpper()
+			} else if (x >= 0.33 && x < 0.66) {
+				password += getLower()
+			} else if (x >= 0.66) {
+				password += getSymbol()
 			}
 		} else {
 			if (x < 0.5) {
