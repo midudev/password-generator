f4dbf54ce105ce507ba4b190f626aac6ccbc363c
Luis Marsiglia <marsiglia.business@gmail.com>
2022-10-08 20:26:24 -0400

    add icons on buttons

--- src/components/marsidev/components/Button.tsx
+++ src/components/marsidev/components/Button.tsx
@@ -1,19 +1,16 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 /** @jsxImportSource solid-js */
-import type { Component, JSX } from 'solid-js'
+import type { ParentComponent, JSX } from 'solid-js'
 
-interface ButtonsProps extends JSX.ButtonHTMLAttributes<HTMLButtonElement> {
-	caption: string
-	handleClick: () => void
-	foo?: string
-}
+type ButtonsProps = JSX.ButtonHTMLAttributes<HTMLButtonElement>
 
-export const Button: Component<ButtonsProps> = ({ caption, handleClick }) => {
+export const Button: ParentComponent<ButtonsProps> = (props) => {
 	return (
 		<button
-			onclick={handleClick}
-			class='w-auto min-w-[180px] rounded-[60px] bg-[#9F7AEA] py-2 px-4 text-lg font-bold leading-8 text-white transition duration-150 ease-in-out hover:scale-105 hover:brightness-90 focus:outline-violet-600 active:scale-95'
+			onClick={props.onClick ? props.onClick : () => {}}
+			class='flex w-auto min-w-[180px] items-center justify-center gap-2 rounded-[60px] bg-[#9F7AEA] py-2 px-4 text-lg font-bold leading-8 text-white transition duration-150 ease-in-out hover:scale-105 hover:brightness-90 focus:outline-violet-600 active:scale-95'
 		>
-			{caption}
+			{props.children}
 		</button>
 	)
 }
--- src/components/marsidev/components/icons/index.ts
+++ src/components/marsidev/components/icons/index.ts
@@ -3,4 +3,6 @@ import Info from './Info'
 import Success from './Success'
 import Warn from './Warn'
 
+export { GenerateIcon } from './GenerateIcon'
+export { CopyIcon } from './CopyIcon'
 export { Error, Info, Success, Warn }
