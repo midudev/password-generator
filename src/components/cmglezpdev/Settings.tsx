ec31673fdde1888ba48f860f42580b85fb5f31e4
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-09 20:02:01 -0400

    feat:add settings hook in the settings component

--- src/components/cmglezpdev/Settings.tsx
+++ src/components/cmglezpdev/Settings.tsx
@@ -1,30 +1,60 @@
+import { SettingsOption } from '@hooks/cmglezpdev/useSettings'
+import { useSettings } from '../../hooks/cmglezpdev/useSettings'
+
+const INITIAL_STATE: SettingsOption[] = [
+	{
+		name: 'letters',
+		checked: true
+	},
+	{
+		name: 'numbers',
+		checked: false
+	},
+	{
+		name: 'div_in_groups',
+		checked: false
+	},
+	{
+		name: 'special_characters',
+		checked: false
+	}
+]
+
 
 export const Settings = () => {
+	const { settings, handleInputChange, findByName, checkAll, checked } = useSettings(INITIAL_STATE)
+
 	return (
 		<div className='mt-6'>
 			<h2 className='text-3xl text-center mb-2 text-white font-bold'>Settings</h2>
 
 			<div className='grid grid-cols-2 w-60'>
-				<div className='col-span-full text-center'>
-					<input type='checkbox' name='strong_password' className='mr-2 outline-none' />
-					<label htmlFor='strong_password' className='text-gray-400 text-xl'>Strong Password</label>
-				</div>
-				<div>
-					<input type='checkbox' name='letters' className='mr-2 outline-none' />
-					<label htmlFor='letters' className='text-gray-400 text-xl'>Letters</label>
-				</div>
-				<div>
-					<input type='checkbox' name='numbers' className='mr-2 outline-none' />
-					<label htmlFor='numbers' className='text-gray-400 text-xl'>Numbers</label>
-				</div>
-				<div>
-					<input type='checkbox' name='div_in_groups' className='mr-2 outline-none' />
-					<label htmlFor='div_in_groups' className='text-gray-400 text-xl'>Divide</label>
-				</div>
-				<div>
-					<input type='checkbox' name='div_in-groups' className='mr-2 outline-none' />
-					<label htmlFor='special_Characters' className='text-gray-400 text-xl'>#%!&@</label>
-				</div>
+				<button
+					className='text-cyan-600 text-xl col-span-full transition-colors outline-none hover:text-cyan-300'
+					onClick={() => checkAll(true)}
+				>
+					Strong Password
+				</button>
+
+				<label htmlFor='letters' className='text-gray-400 text-xl' >
+					<input type='checkbox' name='letters' checked={findByName('letters').checked} className='mr-2 outline-none' onChange={handleInputChange} />
+					Letters
+				</label>
+
+				<label htmlFor='numbers' className='text-gray-400 text-xl'>
+					<input type='checkbox' name='numbers' checked={findByName('numbers').checked} className='mr-2 outline-none' onChange={handleInputChange} />
+					Numbers
+				</label>
+
+				<label htmlFor='div_in_groups' className='text-gray-400 text-xl'>
+					<input type='checkbox' name='div_in_groups' checked={findByName('div_in_groups').checked} className='mr-2 outline-none' onChange={handleInputChange} />
+					Divide
+				</label>
+
+				<label htmlFor='special_characters' className='text-gray-400 text-xl'>
+					<input type='checkbox' name='special_characters' checked={findByName('special_characters').checked} className='mr-2 outline-none' onChange={handleInputChange} />
+					#%!@
+				</label>
 			</div>
 		</div>
 	)
