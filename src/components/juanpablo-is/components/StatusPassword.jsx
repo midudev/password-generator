75b42ef046d002bdaf056aa06db2ebb86650d114
Juan Pablo Barrera <juanpablo.is@outlook.com>
2022-10-09 19:42:32 -0500

    Create a custom hook to show an alert about password complexity

--- src/components/juanpablo-is/components/StatusPassword.jsx
+++ src/components/juanpablo-is/components/StatusPassword.jsx
@@ -1,10 +1,22 @@
+import useStatusPassword from '../hooks/useStatusPassword'
+
 const StatusPassword = () => {
+	const { message, icon: Icon, width, color } = useStatusPassword()
+
 	return (
 		<div className='w-full'>
-			<div className='bg-white h-3 rounded-br-md rounded-bl-md'>
-				<div className='h-full w-1/2 bg-red-800 rounded-br-md rounded-bl-md'></div>
+			<div className='bg-white h-2 rounded-br-md'>
+				<div className='h-full' style={{ width, backgroundColor: color }} />
 			</div>
-			<p className='text-white'>STRONG</p>
+
+			{message && (
+				<p
+					className='w-fit flex items-center text-xl p-2 text-white rounded-bl-md rounded-br-md'
+					style={{ backgroundColor: color }}
+				>
+					{Icon && <Icon />} <b>{message}</b>
+				</p>
+			)}
 		</div>
 	)
 }
