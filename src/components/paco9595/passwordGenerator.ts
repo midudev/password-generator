f97a23f5226af09c3ff1b60265e0dce5195c3028
Francisco Villarreal <paco.villarreal9555@gmail.com>
2022-10-10 19:32:42 -0500

    se agrego posibles tipos de contraseña

--- src/components/paco9595/passwordGenerator.ts
+++ src/components/paco9595/passwordGenerator.ts
@@ -1,5 +1,14 @@
-export const PasswordGenerator = (length: number) => {
-	const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+export const PasswordGenerator = (length: number, characters: string[]) => {
+	let charset = ''
+	characters.forEach((character: string) => {
+		if (character === 'uppercaseLetters') {
+			charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+		} else if (character === 'lowercasesLetters') {
+			charset += 'abcdefghijklmnopqrstuvwxyz'
+		} else {
+			charset += '0123456789'
+		}
+	})
 	let retVal = ''
 	for (let i = 0, n = charset.length; i < length; ++i) {
 		retVal += charset.charAt(Math.floor(Math.random() * n))
