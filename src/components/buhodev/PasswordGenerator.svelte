c8dfa895527b137c742652c49612e465b08c3e22
Carlos Ivanchuk <buhodev@proton.me>
2022-10-12 01:30:16 -0400

    refactor: remove refresh button

--- src/components/buhodev/PasswordGenerator.svelte
+++ src/components/buhodev/PasswordGenerator.svelte
@@ -148,7 +148,7 @@
 			{/if}
 		</button>
 
-		<button
+		<!-- <button
 			on:click={() => (password = generate_password(length, DEFAULT_OPTIONS))}
 			title="Refresh password"
 			class="rounded-md border border-white/30 hover:border-white/60 active:bg-gray-400/20 group flex items-center justify-center p-2.5"
@@ -167,7 +167,7 @@
 					d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
 				/>
 			</svg>
-		</button>
+		</button> -->
 	</div>
 
 	<label for="length" class="mt-8 inline-block w-full">
