5c62ea476a4ba4662a959a967ea142e6d30836aa
eyubkh <eyub.kh@gmail.com>
2022-10-12 21:11:27 +0100

    adding logic function and renaming ids

--- src/components/eyubkh/GeneratePassword.jsx
+++ src/components/eyubkh/GeneratePassword.jsx
@@ -8,44 +8,53 @@ export const GeneratePassword = () => {
 		event.preventDefault()
 		setInitValue(Number(event.target.value))
 
-		if (Number(initValue) >= 12) {
-			window.document.getElementById('castle').style.bottom = '50px'
+		if (initValue >= 14) {
+			window.document.getElementById('eyubkh-fullWall').style.marginBottom = '0px'
 		} else {
-			window.document.getElementById('castle').style.bottom = '0px'
-		}
-		if (initValue <= 8) {
-			window.document.getElementById('tower').style.bottom = '-300px'
-			window.document.getElementById('tower2').style.bottom = '-300px'
-		} else {
-			window.document.getElementById('tower').style.bottom = '0px'
-			window.document.getElementById('tower2').style.bottom = '0px'
-		}
-		if (initValue <= 6) {
-			window.document.getElementById('wall').style.bottom = '-300px'
-		} else {
-			window.document.getElementById('wall').style.bottom = '0px'
+			window.document.getElementById('eyubkh-fullWall').style.marginBottom = '-300px'
 		}
 
-		console.log(initValue)
+		if (Number(initValue) >= 12) {
+			window.document.getElementById('eyubkh-castle').style.marginBottom = '30px'
+		} else {
+			window.document.getElementById('eyubkh-castle').style.marginBottom = '0px'
+		}
+
+		if (initValue <= 8) {
+			const towers = window.document.querySelectorAll('#eyubkh-tower')
+			towers.forEach((tower) => (tower.style.marginBottom = '-300px'))
+		} else {
+			const towers = window.document.querySelectorAll('#eyubkh-tower')
+			towers.forEach((tower) => (tower.style.marginBottom = '0px'))
+		}
+
+		if (initValue <= 6) {
+			window.document.getElementById('eyubkh-wall').style.marginBottom = '-300px'
+		} else {
+			window.document.getElementById('eyubkh-wall').style.marginBottom = '0px'
+		}
 	}
+
 	function handlerGeneratePassword() {
 		setPassword('password generated')
 	}
 
 	return (
-		<div id='form'>
-			<p>{password}</p>
-			<label htmlFor='eyubkh-input'>{initValue}</label>
-			<input
-				id='eyubkh-input'
-				onChange={handlerRange}
-				type='range'
-				value={`${initValue}`}
-				min='5'
-				max='16'
-			/>
+		<div id='eyubkh-generate-card'>
+			<p id='eyubkh-password'>{password}</p>
+			<div>
+				<label htmlFor='eyubkh-input'>{initValue}</label>
+				<input
+					id='eyubkh-input'
+					onChange={handlerRange}
+					type='range'
+					value={`${initValue}`}
+					min='5'
+					max='16'
+				/>
+			</div>
 			<button onClick={handlerGeneratePassword} id='eyubkh-button'>
-				generate password
+				Generate
 			</button>
 		</div>
 	)
