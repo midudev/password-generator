a86404e5d811dcd1f005782107b79138e312b48f
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-13 11:53:57 -0400

    feat(Root): add refact of the components using the basic components

--- src/components/cmglezpdev/CustomPassword.tsx
+++ src/components/cmglezpdev/CustomPassword.tsx
@@ -1,10 +1,46 @@
+import { ChangeEvent, useState } from 'react'
+import { usePassword } from '@hooks/cmglezpdev'
+import { ButtonComponent, RangeInputComponent, InputComponent } from './basic-components'
 import { Settings } from './Settings'
-import { InputPassword } from './InputPassword'
 
 export const CustomPassword = () => {
+	const [lengthPassword, setLenghtPassword] = useState(10)
+	const { password, generatePassword, generatePasswordByPhrase } = usePassword()
+
+	const handleLengthControl = (e:ChangeEvent<HTMLInputElement>) => {
+		setLenghtPassword(parseInt(e.target.value))
+	}
+
+	const handleGenPassword = () => {
+		const set = {
+			length: lengthPassword,
+			letters: true,
+			special: true,
+			numbers: true
+		}
+		generatePassword(set)
+		// generatePasswordByPhrase('Esto es una frase', 10)
+	}
+
+
 	return (
 		<div className='flex flex-col items-center'>
-			<InputPassword />
+			<InputComponent
+				value={password}
+				// eslint-disable-next-line @typescript-eslint/no-empty-function
+				onChange={() => {}}
+			/>
+
+			<RangeInputComponent
+				value={lengthPassword}
+				onChange={handleLengthControl}
+			/>
+
+			<ButtonComponent
+				textButton='Generate Password'
+				onClick={handleGenPassword}
+			/>
+
 			<Settings />
 		</div>
 	)
