a4941ca22169611941a9f8f671e82b8cd347b42d
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-13 11:53:57 -0400

    feat(Root): add refact of the components using the basic components

--- src/components/cmglezpdev/AdvancedPassword.tsx
+++ src/components/cmglezpdev/AdvancedPassword.tsx
@@ -1,9 +1,50 @@
-import { InputPassword } from './InputPassword'
+import { ChangeEvent, useState } from 'react'
+import { usePassword, useField } from '@hooks/cmglezpdev'
+import { ButtonComponent, InputComponent, RangeInputComponent } from './basic-components'
 
 export const AdvancedPassword = () => {
+	const [lengthPassword, setLenghtPassword] = useState(10)
+	const [phrase, setPhrase] = useState('')
+	const { password, generatePassword, generatePasswordByPhrase } = usePassword()
+
+	const handleLengthControl = (e:ChangeEvent<HTMLInputElement>) => {
+		setLenghtPassword(parseInt(e.target.value))
+	}
+
+	const handleGenPassword = () => {
+		const set = {
+			length: lengthPassword,
+			letters: true,
+			special: true,
+			numbers: true
+		}
+		generatePassword(set)
+		// generatePasswordByPhrase('Esto es una frase', 10)
+	}
+
 	return (
 		<div className='flex flex-col items-center'>
-			<InputPassword />
+			<InputComponent
+				value={password}
+				// eslint-disable-next-line @typescript-eslint/no-empty-function
+				onChange={() => {}}
+			/>
+
+			<InputComponent
+				value={phrase}
+				// eslint-disable-next-line @typescript-eslint/no-empty-function
+				onChange={(e) => { setPhrase(e.target.value) }}
+			/>
+
+			<RangeInputComponent
+				value={lengthPassword}
+				onChange={handleLengthControl}
+			/>
+
+			<ButtonComponent
+				textButton='Generate Password'
+				onClick={handleGenPassword}
+			/>
 		</div>
 	)
 }
