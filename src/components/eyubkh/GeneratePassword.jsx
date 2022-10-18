6457d4f2a81eb4d73a8585d279cc5121d5265cc0
eyubkh <eyub.kh@gmail.com>
2022-10-18 13:25:35 +0100

    removing animation logic, adding animation and generate password logic component

--- src/components/eyubkh/GeneratePassword.jsx
+++ src/components/eyubkh/GeneratePassword.jsx
@@ -1,42 +1,21 @@
 import { useState } from 'react'
+import animation from './library/animation'
+import generatePassword from './library/generatePassword'
+
 import './style/generatePassword.css'
 
 export const GeneratePassword = () => {
 	const [initValue, setInitValue] = useState(10)
 	const [password, setPassword] = useState('need a password?')
+
 	function handlerRange(event) {
 		event.preventDefault()
 		setInitValue(Number(event.target.value))
-
-		if (initValue >= 14) {
-			window.document.getElementById('eyubkh-fullWall').style.marginBottom = '0px'
-		} else {
-			window.document.getElementById('eyubkh-fullWall').style.marginBottom = '-300px'
-		}
-
-		if (Number(initValue) >= 12) {
-			window.document.getElementById('eyubkh-castle').style.marginBottom = '30px'
-		} else {
-			window.document.getElementById('eyubkh-castle').style.marginBottom = '0px'
-		}
-
-		if (initValue <= 8) {
-			const towers = window.document.querySelectorAll('#eyubkh-tower')
-			towers.forEach((tower) => (tower.style.marginBottom = '-300px'))
-		} else {
-			const towers = window.document.querySelectorAll('#eyubkh-tower')
-			towers.forEach((tower) => (tower.style.marginBottom = '0px'))
-		}
-
-		if (initValue <= 6) {
-			window.document.getElementById('eyubkh-wall').style.marginBottom = '-300px'
-		} else {
-			window.document.getElementById('eyubkh-wall').style.marginBottom = '0px'
-		}
+		animation(initValue)
 	}
 
 	function handlerGeneratePassword() {
-		setPassword('password generated')
+		setPassword(generatePassword(initValue))
 	}
 
 	return (
@@ -48,7 +27,7 @@ export const GeneratePassword = () => {
 					id='eyubkh-input'
 					onChange={handlerRange}
 					type='range'
-					value={`${initValue}`}
+					value={initValue}
 					min='5'
 					max='16'
 				/>
