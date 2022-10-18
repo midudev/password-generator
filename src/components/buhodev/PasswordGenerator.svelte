d110bd12c95c952c1d2e40e05c2e19089a23a6c6
Carlos Ivanchuk <buhodev@proton.me>
2022-10-18 12:34:53 -0400

    feat: add more backgrounds

--- src/components/buhodev/PasswordGenerator.svelte
+++ src/components/buhodev/PasswordGenerator.svelte
@@ -238,4 +238,14 @@
 	label:has(input[type='checkbox']:hover) {
 		transform: scaleX(1.01) scaleY(1.01);
 	}
+
+	.bg-dots {
+		background-color: #080808;
+		background-image: url("data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%231d1d1d' fill-opacity='0.4' fill-rule='evenodd'%3E%3Ccircle cx='3' cy='3' r='3'/%3E%3Ccircle cx='13' cy='13' r='3'/%3E%3C/g%3E%3C/svg%3E");
+	}
+
+	.bg-texture {
+		background-color: #080808;
+		background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23272727' fill-opacity='0.4' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
+	}
 </style>
