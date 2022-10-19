4712e8b327a5d3e0d5f9241aab343b0c01a5754c
eyubkh <eyub.kh@gmail.com>
2022-10-19 08:57:22 +0100

    adding password component

---
+++ src/components/eyubkh/PasswordContainer.jsx
@@ -0,0 +1,21 @@
+import './style/passwordContainer.css'
+
+export const PasswordContainer = ({ children: password }) => {
+	function handlerCopy() {
+		navigator.clipboard.writeText(password)
+	}
+
+	return (
+		<div id='eyubkh-password'>
+			<p id='eyubkh-password-copy'>{password}</p>
+			<svg
+				onClick={handlerCopy}
+				width='20px'
+				xmlns='http://www.w3.org/2000/svg'
+				viewBox='0 0 512 512'
+			>
+				<path d='M224 0c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224zM64 160c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H288v64H64V224h64V160H64z' />
+			</svg>
+		</div>
+	)
+}
