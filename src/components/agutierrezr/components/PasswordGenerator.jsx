68cf7bc7fa1b5ee55bd1942f1193a7224698a7e3
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2022-10-19 10:11:07 +0200

    Remove Tailwind in favor of native CSS

--- src/components/agutierrezr/components/PasswordGenerator.jsx
+++ src/components/agutierrezr/components/PasswordGenerator.jsx
@@ -27,99 +27,75 @@ const PasswordGenerator = () => {
 				</div>
 
 				<form onSubmit={handleSubmit}>
-					<div className='flex items-center justify-between mb-4'>
-						<label className='text-sm font-medium' htmlFor='length'>
-							Length{' '}
+					<div className='label'>Length: {length}</div>
+					<div className='box'>
+						<div className='slider'>
+							<div className='slider__value--left'>5</div>
 							<input
-								className=''
-								type='number'
+								type='range'
+								className='slider-field'
+								id='length'
+								name='length'
 								min={5}
 								max={128}
 								value={length}
 								onChange={({ target }) => setLength(target.value)}
-							/>{' '}
-						</label>
-						<input
-							type='range'
-							className='h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer'
-							id='length'
-							name='length'
-							min={5}
-							max={128}
-							value={length}
-							onChange={({ target }) => setLength(target.value)}
-						/>
-					</div>
-
-					<div className='flex items-center justify-between mb-4'>
-						<label className='text-sm font-medium' htmlFor='lower'>
-							Minuculas
-						</label>
-						<input
-							className='w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:border-gray-600'
-							type='checkbox'
-							id='lower'
-							name='lower'
-							checked={lower}
-							onChange={({ target }) => setLower(target.checked)}
-						/>
-					</div>
-
-					<div className='flex items-center justify-between mb-4'>
-						<label className=' text-sm font-medium' htmlFor='upper'>
-							Mayusculas
-						</label>
-						<input
-							type='checkbox'
-							className='w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:border-gray-600'
-							id='upper'
-							name='upper'
-							checked={upper}
-							onChange={({ target }) => setUpper(target.checked)}
-						/>
-					</div>
-
-					<div className='flex items-center justify-between mb-4'>
-						<label className='text-sm font-medium' htmlFor='number'>
-							Numeros
-						</label>
-						<input
-							type='checkbox'
-							className='w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:border-gray-600'
-							id='numbers'
-							name='numbers'
-							checked={number}
-							onChange={({ target }) => setNumber(target.checked)}
-						/>
-					</div>
-
-					<div className='flex items-center justify-between mb-4'>
-						<label className='text-sm font-medium' htmlFor='symbol'>
-							Simbolos
-						</label>
-						<input
-							type='checkbox'
-							className='w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:border-gray-600'
-							id='symbol'
-							name='symbol'
-							checked={symbol}
-							onChange={({ target }) => setSymbol(target.checked)}
-						/>
-					</div>
-
-					<div>
-						<label for='checked-toggle' class='inline-flex relative items-center cursor-pointer'>
-							<input
-								type='checkbox'
-								value=''
-								id='checked-toggle'
-								class='sr-only peer'
-								checked={symbol}
-								onChange={({ target }) => setSymbol(target.checked)}
 							/>
-							<div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-							<span class='text-sm font-medium'>Checked toggle</span>
-						</label>
+							<div className='slider__value--right'>128</div>
+						</div>
+						<div className='field'></div>
+					</div>
+
+					<div className='label'>Settings</div>
+					<div className='generator-settings'>
+						<div className='box'>
+							<div className='checkbox'>
+								<label htmlFor='lower'>Minuculas</label>
+								<input
+									type='checkbox'
+									id='lower'
+									name='lower'
+									checked={lower}
+									onChange={({ target }) => setLower(target.checked)}
+								/>
+							</div>
+						</div>
+						<div className='box'>
+							<div className='checkbox'>
+								<label htmlFor='upper'>Mayusculas</label>
+								<input
+									type='checkbox'
+									id='upper'
+									name='upper'
+									checked={upper}
+									onChange={({ target }) => setUpper(target.checked)}
+								/>
+							</div>
+						</div>
+						<div className='box'>
+							<div className='checkbox'>
+								<label htmlFor='numbers'>Numeros</label>
+								<input
+									type='checkbox'
+									id='numbers'
+									name='numbers'
+									checked={number}
+									onChange={({ target }) => setNumber(target.checked)}
+								/>
+							</div>
+						</div>
+						<div className='box'>
+							<div className='checkbox'>
+								<label htmlFor='symbol'>Simbolos</label>
+								<input
+									type='checkbox'
+									id='symbol'
+									name='symbol'
+									checked={symbol}
+									onChange={({ target }) => setSymbol(target.checked)}
+								/>
+							</div>
+						</div>
 					</div>
 
 					<button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full'>
