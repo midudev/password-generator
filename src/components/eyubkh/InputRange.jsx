6e208de3acaf4e4e141d1f6fd220179cec42696f
eyubkh <eyub.kh@gmail.com>
2022-10-19 08:56:53 +0100

    adding input component

---
+++ src/components/eyubkh/InputRange.jsx
@@ -0,0 +1,17 @@
+import './style/inputRange.css'
+
+export const InputRange = ({ handler, value }) => {
+	return (
+		<div id='eyubkh-input'>
+			<label htmlFor='eyubkh-input-range'>{value <= 9 ? '0' + value : value}</label>
+			<input
+				id='eyubkh-input-range'
+				onChange={handler}
+				type='range'
+				value={value}
+				min='5'
+				max='16'
+			/>
+		</div>
+	)
+}
