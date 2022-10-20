026a8235deb553b3b5eb2572cd81e08fcc83e42b
eyubkh <eyub.kh@gmail.com>
2022-10-20 19:51:52 +0100

    adding handler into component

--- src/components/eyubkh/Input.jsx
+++ src/components/eyubkh/Input.jsx
@@ -1,12 +1,19 @@
+import animation from './utils/animation'
+
 import './style/input.css'
 
-export const Input = ({ handler, value }) => {
+export const Input = ({ value, setValue }) => {
+	function handlerRange(event) {
+		event.preventDefault()
+		setValue(Number(event.target.value))
+		animation(value)
+	}
 	return (
 		<div id='eyubkh-input'>
 			<label htmlFor='eyubkh-input-range'>{value <= 9 ? '0' + value : value}</label>
 			<input
 				id='eyubkh-input-range'
-				onChange={handler}
+				onChange={handlerRange}
 				type='range'
 				value={value}
 				min='5'
