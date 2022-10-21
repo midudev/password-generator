b05a6d53ac3d8874e1b0f96e39220a08c6905c3b
Dani <danizt96@gmail.com>
2022-10-21 23:14:29 +0200

    Add GitHub logo

--- src/components/danizt/components/Wrapper.jsx
+++ src/components/danizt/components/Wrapper.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import PasswordGenerator from './PasswordGenerator'
+import GitHubSvg from '../icons/gitHubSvg'
 
 const Wrapper = () => {
 	return (
 		<div className='text-white flex flex-col items-center justify-center h-screen font-sans'>
 			<PasswordGenerator />
+
+			<p className='text-white font-mono absolute bottom-5'>
+				<a
+					href='https://github.com/danizt'
+					title='danizt Github profile'
+					target='_blank'
+					className='flex flex-row gap-2'
+				>
+					by @danizt <GitHubSvg />
+				</a>
+			</p>
 		</div>
 	)
 }
