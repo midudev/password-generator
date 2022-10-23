1a540498d7f4c30d87471728e1f66c4dd15381ad
Juan Pablo Barrera <juanpablo.is@outlook.com>
2022-10-23 16:57:07 -0500

    Add table to HistoryPassword to list all password saved storage

--- src/components/juanpablo-is/components/HistoryPassword.jsx
+++ src/components/juanpablo-is/components/HistoryPassword.jsx
@@ -3,19 +3,76 @@ import Drawer from './common/Drawer'
 import IconChevronsDown from './common/icons/IconChevronsDown'
 
 const HistoryPassword = () => {
-	const [showDrawer, setShowDrawer] = useState(false)
+	const [showDrawer, setShowDrawer] = useState(true)
+
+	const passwords = [
+		{ password: '{fxo1~_O>vB_3j95&Nz0U_~75C', date: +new Date() },
+		{ password: 'd8f7RsSL2135eM6x5AhkWIZ96Z', date: +new Date() },
+		{ password: 'MmtS0dZSVR568laF1qEdUcX1K7', date: +new Date() }
+	]
 
 	const handlerShowDrawer = () => {
 		setShowDrawer((prev) => !prev)
 	}
 
+	const handlerSetPassword = (password) => {}
+
 	return (
 		<>
 			<div className='flex flex-col items-center py-2 text-white' onClick={handlerShowDrawer}>
 				<IconChevronsDown />
-				<p className='font-bold capitalize'>Show history password...</p>
+				<p className='font-bold capitalize'>Show history password</p>
 			</div>
-			{showDrawer && <Drawer onClose={() => setShowDrawer(false)} />}
+
+			{showDrawer && (
+				<Drawer title='History password generated' onClose={() => setShowDrawer(false)}>
+					<div className='overflow-x-auto'>
+						{passwords.length > 0 ? (
+							<table className='w-full text-sm relative shadow-md sm:rounded-lg text-left text-gray-500 dark:text-gray-400'>
+								<thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+									<tr>
+										<th scope='col' className='py-3 px-6'>
+											Password
+										</th>
+										<th scope='col' className='py-3 px-6 text-center'>
+											Date
+										</th>
+										<th scope='col' className='py-3 px-6 text-end'>
+											Set password
+										</th>
+									</tr>
+								</thead>
+								<tbody>
+									{passwords.map(({ password, date }) => (
+										<tr
+											key={password}
+											className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+										>
+											<th
+												scope='row'
+												className='py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+											>
+												{password}
+											</th>
+											<td className='py-4 px-6 text-center'>{new Date(date).toISOString()}</td>
+											<td className='py-4 px-6 text-right'>
+												<button
+													className='font-medium text-blue-600 dark:text-blue-500 hover:underline'
+													onClick={() => handlerSetPassword(password)}
+												>
+													SET
+												</button>
+											</td>
+										</tr>
+									))}
+								</tbody>
+							</table>
+						) : (
+							<p className='font-bold capitalize'>Not password generated...</p>
+						)}
+					</div>
+				</Drawer>
+			)}
 		</>
 	)
 }
--- src/components/juanpablo-is/components/common/Drawer.jsx
+++ src/components/juanpablo-is/components/common/Drawer.jsx
@@ -1,4 +1,4 @@
-const Drawer = ({ onClose }) => {
+const Drawer = ({ title, onClose, children }) => {
 	return (
 		<>
 			<div
@@ -7,20 +7,7 @@ const Drawer = ({ onClose }) => {
 				aria-labelledby='drawer-bottom-label'
 			>
 				<h5 className='inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400'>
-					<svg
-						className='w-5 h-5 mr-2'
-						aria-hidden='true'
-						fill='currentColor'
-						viewBox='0 0 20 20'
-						xmlns='http://www.w3.org/2000/svg'
-					>
-						<path
-							fillRule='evenodd'
-							d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z'
-							clipRule='evenodd'
-						></path>
-					</svg>
-					Bottom drawer
+					{title}
 				</h5>
 				<button
 					type='button'
@@ -44,24 +31,17 @@ const Drawer = ({ onClose }) => {
 					</svg>
 					<span className='sr-only'>Close menu</span>
 				</button>
-				<p className='max-w-lg mb-6 text-sm text-gray-500 dark:text-gray-400'>
-					Supercharge your hiring by taking advantage of our{' '}
-					<a href='#' className='text-blue-600 underline dark:text-blue-500 hover:no-underline'>
-						limited-time sale
-					</a>{' '}
-					for Flowbite Docs + Job Board. Unlimited access to over 190K top-ranked candidates and the
-					#1 design job board.
-				</p>
-				<a
-					href='#'
+
+				<hr />
+
+				<div className='my-6 text-sm text-gray-500 dark:text-gray-400'>{children}</div>
+				<button
+					onClick={onClose}
 					className='px-4 py-2 mr-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'
 				>
-					Learn more
-				</a>
-				<a
-					href='#'
-					className='inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
-				>
+					Cancel
+				</button>
+				<button className='inline-flex hidden items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
 					Get access{' '}
 					<svg
 						className='w-4 h-4 ml-2'
@@ -76,7 +56,7 @@ const Drawer = ({ onClose }) => {
 							clipRule='evenodd'
 						></path>
 					</svg>
-				</a>
+				</button>
 			</div>
 			<div
 				className='bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-30'
