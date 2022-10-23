7d1e90b0e55c2733c0ad6704b1b167dd430441f0
Carlos Ivanchuk <buhodev@proton.me>
2022-10-23 00:12:08 -0400

    feat: improve UI of saved passwords & add title

--- src/components/buhodev/PasswordGenerator.svelte
+++ src/components/buhodev/PasswordGenerator.svelte
@@ -269,15 +269,79 @@
 			out:fly={{ x: 250, opacity: 1, easing: quadIn, duration: 100 }}
 			class="fixed overflow-y-auto top-0 right-0 bottom-0 mt-12 flex w-72 flex-col bg-neutral-900 border-l border-l-gray-600 text-white"
 		>
-			<div class="flex px-2">
-				<div class="flex flex-col">
-					<span class="">••••••••••••</span>
-					<span class="text-sm whitespace-nowrap">Generated on Nov 1, 14:00</span>
-				</div>
-				<div class="flex">
-					<button on:click={() => (closeEye = !closeEye)}>
-						{#if closeEye}
-							<!-- phosphoricons/eye -->
+			<span class="px-4 mt-6 text-neutral-400 text-xs font-medium">SAVED PASSWORDS</span>
+
+			{#each Array(8) as _, i}
+				<div
+					class="hover:bg-neutral-800 rounded-md flex mx-2 px-2 py-1.5 mt-3 justify-between items-center"
+				>
+					<div class="flex flex-col">
+						<span class="">••••••••••••</span>
+						<span class="text-sm text-neutral-300 whitespace-nowrap"
+							>Generated on Nov {i + 1}, 1{i + 2}:00</span
+						>
+					</div>
+					<div class="flex gap-2">
+						<button on:click={() => (closeEye = !closeEye)}>
+							{#if closeEye}
+								<!-- phosphoricons/eye -->
+								<svg
+									width="20"
+									height="20"
+									viewBox="0 0 24 24"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										d="M12 5.25C4.5 5.25 1.5 12 1.5 12s3 6.75 10.5 6.75S22.5 12 22.5 12s-3-6.75-10.5-6.75Z"
+										stroke="#fff"
+										stroke-width="1.5"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+									/>
+									<path
+										d="M12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"
+										stroke="#fff"
+										stroke-width="1.5"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+									/>
+								</svg>
+							{:else}
+								<!-- phosphoricons/eye-slash -->
+								<svg
+									width="20"
+									height="20"
+									viewBox="0 0 24 24"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										d="m4.5 3.75 15 16.5m-4.978-5.475a3.75 3.75 0 0 1-5.044-5.55"
+										stroke="#fff"
+										stroke-width="1.5"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+									/>
+									<path
+										d="M6.938 6.431C3.112 8.363 1.5 12 1.5 12s3 6.75 10.5 6.75c1.757.014 3.493-.39 5.063-1.181m2.493-1.716C21.6 14.025 22.5 12 22.5 12s-3-6.75-10.5-6.75c-.65-.001-1.3.052-1.94.16"
+										stroke="#fff"
+										stroke-width="1.5"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+									/>
+									<path
+										d="M12.703 8.316a3.74 3.74 0 0 1 3.028 3.328"
+										stroke="#fff"
+										stroke-width="1.5"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+									/>
+								</svg>
+							{/if}
+						</button>
+						<button>
+							<!-- phosphoricons/copy -->
 							<svg
 								width="20"
 								height="20"
@@ -286,22 +350,23 @@
 								xmlns="http://www.w3.org/2000/svg"
 							>
 								<path
-									d="M12 5.25C4.5 5.25 1.5 12 1.5 12s3 6.75 10.5 6.75S22.5 12 22.5 12s-3-6.75-10.5-6.75Z"
+									d="M15.75 15.75h4.5v-12h-12v4.5"
 									stroke="#fff"
 									stroke-width="1.5"
 									stroke-linecap="round"
 									stroke-linejoin="round"
 								/>
 								<path
-									d="M12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"
+									d="M15.75 8.25h-12v12h12v-12Z"
 									stroke="#fff"
 									stroke-width="1.5"
 									stroke-linecap="round"
 									stroke-linejoin="round"
 								/>
 							</svg>
-						{:else}
-							<!-- phosphoricons/eye-slash -->
+						</button>
+						<button>
+							<!-- phosphoricons/trash -->
 							<svg
 								width="20"
 								height="20"
@@ -310,74 +375,17 @@
 								xmlns="http://www.w3.org/2000/svg"
 							>
 								<path
-									d="m4.5 3.75 15 16.5m-4.978-5.475a3.75 3.75 0 0 1-5.044-5.55"
-									stroke="#fff"
-									stroke-width="1.5"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-								/>
-								<path
-									d="M6.938 6.431C3.112 8.363 1.5 12 1.5 12s3 6.75 10.5 6.75c1.757.014 3.493-.39 5.063-1.181m2.493-1.716C21.6 14.025 22.5 12 22.5 12s-3-6.75-10.5-6.75c-.65-.001-1.3.052-1.94.16"
-									stroke="#fff"
-									stroke-width="1.5"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-								/>
-								<path
-									d="M12.703 8.316a3.74 3.74 0 0 1 3.028 3.328"
+									d="M20.25 5.25H3.75m6 4.5v6m4.5-6v6m4.5-10.5V19.5a.75.75 0 0 1-.75.75H6a.75.75 0 0 1-.75-.75V5.25m10.5 0v-1.5a1.5 1.5 0 0 0-1.5-1.5h-4.5a1.5 1.5 0 0 0-1.5 1.5v1.5"
 									stroke="#fff"
 									stroke-width="1.5"
 									stroke-linecap="round"
 									stroke-linejoin="round"
 								/>
 							</svg>
-						{/if}
-					</button>
-					<button>
-						<!-- phosphoricons/copy -->
-						<svg
-							width="20"
-							height="20"
-							viewBox="0 0 24 24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M15.75 15.75h4.5v-12h-12v4.5"
-								stroke="#fff"
-								stroke-width="1.5"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-							<path
-								d="M15.75 8.25h-12v12h12v-12Z"
-								stroke="#fff"
-								stroke-width="1.5"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-						</svg>
-					</button>
-					<button>
-						<!-- phosphoricons/trash -->
-						<svg
-							width="20"
-							height="20"
-							viewBox="0 0 24 24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M20.25 5.25H3.75m6 4.5v6m4.5-6v6m4.5-10.5V19.5a.75.75 0 0 1-.75.75H6a.75.75 0 0 1-.75-.75V5.25m10.5 0v-1.5a1.5 1.5 0 0 0-1.5-1.5h-4.5a1.5 1.5 0 0 0-1.5 1.5v1.5"
-								stroke="#fff"
-								stroke-width="1.5"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-						</svg>
-					</button>
+						</button>
+					</div>
 				</div>
-			</div>
+			{/each}
 		</aside>
 	{/if}
 </div>
