78ae6f9029f10c4499934138b80f62c72de890f4
Freddy González <7684330+fredoist@users.noreply.github.com>
2022-10-24 02:10:36 -0500

    feat(fredoist): add checkboxes component to update password options
    add multiple checkboxes to set the options of the password to be generated

---
+++ src/components/fredoist/Checkboxes.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { useOptions } from '@hooks/fredoist/use-options'
+
+	const options = useOptions()
+	const checkboxes = Object.keys($options).slice(1)
+
+	async function updateCheckbox(event: Event) {
+		const input = event.target as HTMLInputElement
+		const { name, checked } = input
+		console.log(name, checked)
+		options.update({ [name]: checked })
+	}
+</script>
+
+<div class="grid grid-cols-2 gap-5">
+	{#each checkboxes as checkbox}
+		<label class="inline-flex items-center gap-x-2 capitalize">
+			<input
+				type="checkbox"
+				name={checkbox}
+				checked={$options[checkbox]}
+				class="appearance-none w-6 h-6 border-2 border-[#f8ef00] checked:bg-[#f8ef00]"
+				on:change={updateCheckbox}
+			/>
+			{checkbox}
+		</label>
+	{/each}
+</div>
