d1834b14d35d66728bf3e87a732d25f11b2e1d18
Freddy González <7684330+fredoist@users.noreply.github.com>
2022-10-24 03:19:00 -0500

    feat(fredoist): update layout for desktop

--- src/components/fredoist/PasswordHistory.svelte
+++ src/components/fredoist/PasswordHistory.svelte
@@ -2,22 +2,34 @@
 	import { useHistory } from '@hooks/fredoist/use-history'
 
 	const history = useHistory()
+
+	async function copyPassword(password: string) {
+		await navigator.clipboard.writeText(password)
+	}
 </script>
 
-<div>
+<div class="max-w-sm">
 	<h3 class="uppercase text-xl font-semibold tracking-widest">Password History</h3>
-	<div class="my-8 flex flex-col gap-5 overflow-y-auto max-h-64">
+	<div class="password-history relative my-8 flex flex-col gap-5 overflow-y-auto max-h-56">
 		{#each $history as password}
 			<div class="flex items-center justify-between gap-5">
 				<span class="truncate flex-1">{password}</span>
-				<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 fill-current" viewBox="0 0 512 512">
-					<path
-						d="M456 480H136a24 24 0 01-24-24V128a16 16 0 0116-16h328a24 24 0 0124 24v320a24 24 0 01-24 24z"
-					/>
-					<path
-						d="M112 80h288V56a24 24 0 00-24-24H60a28 28 0 00-28 28v316a24 24 0 0024 24h24V112a32 32 0 0132-32z"
-					/>
-				</svg>
+				<button type="button" on:click={() => copyPassword(password)}>
+					<span class="sr-only">Copy password</span>
+					<svg
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						class="w-6 h-6 fill-current"
+						viewBox="0 0 512 512"
+					>
+						<path
+							d="M456 480H136a24 24 0 01-24-24V128a16 16 0 0116-16h328a24 24 0 0124 24v320a24 24 0 01-24 24z"
+						/>
+						<path
+							d="M112 80h288V56a24 24 0 00-24-24H60a28 28 0 00-28 28v316a24 24 0 0024 24h24V112a32 32 0 0132-32z"
+						/>
+					</svg>
+				</button>
 			</div>
 		{/each}
 	</div>
@@ -29,3 +41,9 @@
 		>
 	{/if}
 </div>
+
+<style>
+	.password-history::-webkit-scrollbar {
+		width: 0.25rem;
+	}
+</style>
