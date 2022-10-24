dc10fb59f35165677da39b72d0a1f890760b4e5d
Eduardo Guette <eduardoguette@gmail.com>
2022-10-24 23:03:45 +0200

    new design

--- src/components/eduardoguette/PGApp.jsx
+++ src/components/eduardoguette/PGApp.jsx
@@ -1,18 +1,74 @@
 import React, { useState } from 'react'
+import logoHack from './assets/logo.jpg'
 import { Form } from './components/Form'
 import { AppContext } from './context'
-
+import './index.css'
 function PGApp() {
 	const [state, setState] = useState({
 		password: ''
 	})
 	return (
 		<AppContext.Provider value={{ state, setState }}>
-			<main className='flex justify-center items-center flex-col min-h-screen bg-[rgb(8,7,11)]'>
-				<h1 className='text-white font-mono  text-center text-3xl font-semibold mb-10'>
-					Password Generator
-				</h1>
-				<Form />
+			<header className='header-eg'></header>
+			<main className='flex selection:bg-[#b4ff39] selection:text-[#170f1e] justify-center items-center flex-col h-screen main-eg mx-auto w-screen p-5'>
+				<header className=''>
+					<a
+						href='/'
+						className='group header-logo-eg fixed font-mono  top-14 h-5 left-5 saturate-50 flex items-center text-white gap-4 hover:saturate-100'
+					>
+						<img src={logoHack.src} alt='Logo hacktober fest' width={50} />
+						<h1 className='tracking-wide	text-lg shadow-eg'>HACKTOBERFEST</h1>
+					</a>
+				</header>
+				<section className='flex justify-center mt-20 md:mt-10 items-center flex-col w-full'>
+					<h1 className='text-white font-mono  text-center text-2xl md:text-4xl font-semibold  md:mb-16 mb-8 bg-clip-text text-transparent bg-gradient-to-r from-[rgb(255,215,77)] via-[rgb(64,221,255)] to-[rgb(124,127,255)]'>
+						Password Generator
+					</h1>
+					<Form />
+				</section>
+				<footer className='text-center mt-4 text-white text-opacity-50'>
+					Desarrollado por Eduardo Guette
+					<div className='flex justify-center gap-2 mt-2'>
+						<a className='text-white opacity-50 hover:opacity-100' href='https://www.linkedin.com/in/eduardo-guette/' target='_blank'>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								class='icon icon-tabler icon-tabler-brand-linkedin'
+								width='24'
+								height='24'
+								viewBox='0 0 24 24'
+								stroke-width='2'
+								stroke='currentColor'
+								fill='none'
+								stroke-linecap='round'
+								stroke-linejoin='round'
+							>
+								<path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+								<rect x='4' y='4' width='16' height='16' rx='2'></rect>
+								<line x1='8' y1='11' x2='8' y2='16'></line>
+								<line x1='8' y1='8' x2='8' y2='8.01'></line>
+								<line x1='12' y1='16' x2='12' y2='11'></line>
+								<path d='M16 16v-3a2 2 0 0 0 -4 0'></path>
+							</svg>
+						</a>
+						<a className='text-white opacity-50 hover:opacity-100' target='_blank' href='https://github.com/eduardoguette?tab=repositories'>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								class='icon icon-tabler icon-tabler-brand-github'
+								width='24'
+								height='24'
+								viewBox='0 0 24 24'
+								stroke-width='2'
+								stroke='currentColor'
+								fill='none'
+								stroke-linecap='round'
+								stroke-linejoin='round'
+							>
+								<path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+								<path d='M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5'></path>
+							</svg>
+						</a>
+					</div>
+				</footer>
 			</main>
 		</AppContext.Provider>
 	)
--- src/components/eduardoguette/components/Form.jsx
+++ src/components/eduardoguette/components/Form.jsx
@@ -68,9 +68,9 @@ export const Form = () => {
 				: 'bg-green-400'
 
 	return (
-		<section className='text-white font-mono w-[470px]'>
+		<section className='text-white font-mono w-[min(470px,100%)] mx-auto'>
 			<label htmlFor='pass' className='block w-full relative text-white mb-8'>
-				<div className='px-6 w-full font-mono py-4 rounded focus:outline-none text-2xl h-[64px] bg-[rgb(36,35,44)] text-[#8e7f9f] eg_input'>
+				<div className='px-6 w-full font-mono py-4 rounded focus:outline-none text-lg md:text-2xl h-[64px] bg-[rgb(36,35,44)] text-[#8e7f9f] eg_input'>
 					{password}
 				</div>
 				<div
@@ -114,9 +114,9 @@ export const Form = () => {
 						<span className='absolute top-6 text-xs right-3 bottom-0 my-auto'>Copied!</span>
 					)}
 			</label>
-			<div className='px-6 py-8 bg-[rgb(36,35,44)]'>
+			<div className='px-6 py-8 bg-[rgb(36,35,44)] '>
 				<label htmlFor='uppercase' className='flex items-center justify-between'>
-					<p className=''>Include Uppercase Letters </p>
+					<p className='text-sm md:text-base'>Include Uppercase Letters </p>
 					<input
 						className='border scale-150 eg_input'
 						onChange={handleInputCheckboxChange}
@@ -127,7 +127,7 @@ export const Form = () => {
 					/>
 				</label>
 				<label htmlFor='lowercase' className='mt-4 flex items-center justify-between'>
-					<p className=''>Include Lowercase Letters</p>
+					<p className='text-sm md:text-base'>Include Lowercase Letters</p>
 					<input
 						className='border scale-150 eg_input'
 						onChange={handleInputCheckboxChange}
@@ -139,7 +139,7 @@ export const Form = () => {
 					/>
 				</label>
 				<label htmlFor='numbers' className='mt-4 flex items-center justify-between'>
-					<p className=''>Include Numbers</p>
+					<p className='text-sm md:text-base'>Include Numbers</p>
 					<input
 						className='border scale-150 eg_input'
 						onChange={handleInputCheckboxChange}
@@ -151,7 +151,7 @@ export const Form = () => {
 					/>
 				</label>
 				<label htmlFor='uppercase' className='mt-4 flex items-center justify-between'>
-					<p className=''>Include Symbols</p>
+					<p className='text-sm md:text-base'>Include Symbols</p>
 					<input
 						className='border scale-150 eg_input'
 						onChange={handleInputCheckboxChange}
@@ -163,8 +163,8 @@ export const Form = () => {
 				</label>
 				<label htmlFor='long' className='mt-4 block'>
 					<div className='flex items-center justify-between mb-2'>
-						<span>Password Length</span>
-						<span className='text-2xl rounded-md text-[rgb(164,255,175)]'>{lengthPass}</span>
+						<span className='text-sm md:text-base'>Password Length</span>
+						<span className='md:text-2xl rounded-md text-[rgb(164,255,175)]'>{lengthPass}</span>
 					</div>
 					<input
 						className='input_eduardoguette w-full h-1 appearance-none focus:outline-violet-500 '
@@ -180,19 +180,10 @@ export const Form = () => {
 				<button
 					onClick={handleClickNewPass}
 					role='button'
-					className='w-full flex justify-center items-center font-semibold text-lg my-auto focus:outline-none bg-[rgb(164,255,175)] p-4 text-black mt-8'
+					className='w-full rounded-md flex justify-center items-center btn-clip-eg text-md my-auto focus:outline-none bg-gradient-to-r from-[rgb(255,215,77)] via-[rgb(64,221,255)] to-[rgb(124,127,255)] hover:bg-gradient-to-r  tracking-widest drop-shadow-eg p-3 text-black mt-8 hover:rotate-3 transition-all relative group'
 				>
-					Generate
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						fill='none'
-						viewBox='0 0 24 24'
-						strokeWidth={2}
-						stroke='currentColor'
-						className='w-5 h-5'
-					>
-						<path strokeLinecap='round' strokeLinejoin='round' d='M12 6v12m6-6H6' />
-					</svg>
+					GENERATE
+					<div className='clip-btn-eg opacity-10 group-hover:opacity-100 left-0 group-hover:left-12 transition-all duration-700 h-16 w-20 absolute bg-gradient-to-l to-[#ffffff24] from-[#ffffff5e] drop-shadow'/>
 				</button>
 			</div>
 		</section>
