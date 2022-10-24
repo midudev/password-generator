2f768d1ab282d6ef559ca1931a70e54aef88cc86
Carlos Ivanchuk <buhodev@proton.me>
2022-10-24 13:38:55 -0400

    feat: add addPassword & deletePassword methods

--- src/components/buhodev/stores/history.ts
+++ src/components/buhodev/stores/history.ts
@@ -1,7 +1,20 @@
 import { writable } from './localStorage'
 
-export const SAVED_PASSWORDS = writable('saved_passwords', [
+type PasswordEntry = {
+	password: string
+	generated: string
+}
+
+export const savedPasswords = writable('saved_passwords', [
 	{ password: 'j8a6!dzZ^2#A', generated: 'Nov 1, 12:00' },
 	{ password: 'j8a6!dzZ^2#A', generated: 'Nov 2, 12:00' },
 	{ password: 'j8a6!dzZ^2#A', generated: 'Nov 3, 12:00' }
 ])
+
+export const addPassword = (passwordEntry: PasswordEntry) => {
+	savedPasswords.update((all) => [{ ...passwordEntry }, ...all])
+}
+
+export const deletePassword = (password: PasswordEntry['password']) => {
+	savedPasswords.update((all) => all.filter((passwordEntry) => passwordEntry.password !== password))
+}
