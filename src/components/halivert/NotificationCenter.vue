886d4dc4cdbddff5aa5bb1b47cec9f18922ae7ae
Halí V <halivelazquez@outlook.com>
2022-10-25 10:35:28 -0500

    feat: add copy button

--- src/components/halivert/NotificationCenter.vue
+++ src/components/halivert/NotificationCenter.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+const appearSeconds = 2,
+	dissapearSeconds = 4,
+	animationDuration = 1
+
 const notifications = ref([])
+const notificationQueue = ref([])
+
+const timers = new Map()
 
 const notification = (evt: CustomEvent) => {
-	console.log(evt)
-	notifications.value.unshift({
-		type: evt.detail.type,
-		text: evt.detail.text
-	})
+	// TODO: Add notifications
+	// notifications.value.unshift({
+	// 	type: evt.detail.type,
+	// 	text: evt.detail.text
+	// })
 }
 </script>
 
 <template>
-	<div @notify="notification">
+	<div @notify="notification" class="notification-center">
 		<div class="notification" :class="notification.type" v-for="notification in notifications">
 			{{ notification.text }}
 		</div>
@@ -21,20 +28,29 @@ const notification = (evt: CustomEvent) => {
 </template>
 
 <style scoped>
-div {
-	z-index: 1;
+div.notification-center {
+	z-index: 10;
 	position: fixed;
 	bottom: 1em;
 	right: 0.5em;
 	width: 10em;
+	max-width: 100%;
 	max-height: 100vh;
 	display: flex;
+	flex-flow: column-reverse nowrap;
+	overflow: hidden;
+	gap: 0.5em;
 }
 
 .notification {
-	/* background-color: var(--primary); */
 	padding: 0.5em;
 	border-radius: 0.5em;
-	/* color: var(--background); */
+	background-color: var(--background-notify);
+	color: var(--color-notify);
+}
+
+.notification.success {
+	--background-notify: var(--primary);
+	--color-notify: var(--background);
 }
 </style>
