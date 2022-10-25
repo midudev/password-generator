b787df3fd4db350aaaa51d325cff12e854f8d778
mvpsdvlpr <mvpdvlp@gmail.com>
2022-10-25 19:42:24 -0300

    Adding pages Display and Header

---
+++ src/components/mvpsdvlpr/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './styles/Header.css'
+const Header = () => {
+	return (
+		<div className='row'>
+			<div className='col-md-12 header'>
+				<h1 className='h1'>Strong Password Generator</h1>
+				<div className='col-md-12'>
+					<h4>Create strong passwords with Password Generator</h4>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Header
