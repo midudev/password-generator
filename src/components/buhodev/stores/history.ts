d9f0e3c60c1558de971ac1134c001b978fd1ea2c
Carlos Ivanchuk <buhodev@proton.me>
2022-10-25 13:18:04 -0400

    refactor: remove dummy data

--- src/components/buhodev/stores/history.ts
+++ src/components/buhodev/stores/history.ts
@@ -5,11 +5,7 @@ type PasswordEntry = {
 	generated: string
 }
 
-export const savedPasswords = writable('saved_passwords', [
-	{ password: 'j8a6!dzZ^2#A', generated: 'Nov 1, 12:00' },
-	{ password: 'j8a6!dzZ^2#A', generated: 'Nov 2, 12:00' },
-	{ password: 'j8a6!dzZ^2#A', generated: 'Nov 3, 12:00' }
-])
+export const savedPasswords = writable('saved_passwords', [])
 
 export const addPassword = (passwordEntry: PasswordEntry) => {
 	savedPasswords.update((all) => [{ ...passwordEntry }, ...all])
