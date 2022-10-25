b32f4eef814d031cd6a40ea7c437b0fe0bc121e4
mvpsdvlpr <mvpdvlp@gmail.com>
2022-10-25 19:42:42 -0300

    Adding main page

---
+++ src/components/mvpsdvlpr/Generatorpsswrd.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Display from './Display'
+import Header from './Header'
+
+const Generatorpsswrd = () => {
+	return (
+		<>
+			<Header />
+			<Display />
+		</>
+	)
+}
+
+export default Generatorpsswrd
