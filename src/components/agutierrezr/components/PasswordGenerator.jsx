1b7a5357841d821a90fc6c74525d1e8da7eeb4af
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2022-10-25 17:29:45 +0200

    Improve layout, and history

--- src/components/agutierrezr/components/PasswordGenerator.jsx
+++ src/components/agutierrezr/components/PasswordGenerator.jsx
@@ -15,14 +15,14 @@ const PasswordGenerator = () => {
 	const [password, setPassword] = useState(firstPassword)
 	const [showMessage, setShowMessage] = useState(false)
 	const [showHistory, setShowHistory] = useState(false)
-	const [history, setHistory] = useState([])
+	const [history, setHistory] = useState([firstPassword])
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
 		const passwordGenerated = generatePassword(config)
 
 		setPassword(passwordGenerated)
-		setHistory([...history, passwordGenerated])
+		setHistory([passwordGenerated, ...history])
 	}
 
 	const handleCopyPass = () => {
@@ -38,108 +38,116 @@ const PasswordGenerator = () => {
 	return (
 		<div className='site'>
 			<div className='app-container'>
-				{!showHistory && (
-					<form onSubmit={handleSubmit}>
-						<section className='section section--password'>
-							<div className='label'>Generated Password</div>
-							<div className='box box--password'>
-								<div
-									className='password'
-									dangerouslySetInnerHTML={{ __html: colorPassword(password) }}
-								/>
-								<div className='controls'>
-									<button type='submit'>
-										<RefreshIcon />
-									</button>
-									<button type='button' onClick={handleCopyPass}>
-										<CopyIcon />
-									</button>
+				<div className='app-main'>
+					{!showHistory && (
+						<form onSubmit={handleSubmit}>
+							<section className='section section--password'>
+								<div className='label'>Generated Password</div>
+								<div className='box box--password'>
+									<div
+										className='password'
+										dangerouslySetInnerHTML={{ __html: colorPassword(password) }}
+									/>
+									<div className='controls'>
+										<button type='submit'>
+											<RefreshIcon />
+										</button>
+										<button type='button' onClick={handleCopyPass}>
+											<CopyIcon />
+										</button>
+									</div>
 								</div>
-							</div>
-							{showMessage && <span className='copy-message'>Password copied!!!</span>}
-						</section>
-
-						<section className='section'>
-							<div className='label'>Length: {config.length}</div>
-							<div className='box'>
-								<Slider
-									value={config.length}
-									onChange={({ target }) => dispatch({ type: 'length', payload: target.value })}
-									min={5}
-									max={128}
-								/>
-							</div>
-						</section>
-
-						<section className='section'>
-							<div className='label'>Settings</div>
-							<div className='generator-settings'>
+								{showMessage && <span className='copy-message'>Password copied!!!</span>}
+							</section>
+							<section className='section'>
+								<div className='label'>Length: {config.length}</div>
 								<div className='box'>
-									<Toggle
-										label='Include lowercase letters'
-										id='lower'
-										name='lower'
-										checked={config.lower}
-										onChange={() => dispatch({ type: 'lower' })}
+									<Slider
+										value={config.length}
+										onChange={({ target }) => dispatch({ type: 'length', payload: target.value })}
+										min={5}
+										max={128}
 									/>
 								</div>
-								<div className='box'>
-									<Toggle
-										label='Include uppercase letters'
-										id='upper'
-										name='upper'
-										checked={config.upper}
-										onChange={() => dispatch({ type: 'upper' })}
+							</section>
+							<section className='section'>
+								<div className='label'>Settings</div>
+								<div className='generator-settings'>
+									<div className='box'>
+										<Toggle
+											label='Include lowercase letters'
+											id='lower'
+											name='lower'
+											checked={config.lower}
+											onChange={() => dispatch({ type: 'lower' })}
+										/>
+									</div>
+									<div className='box'>
+										<Toggle
+											label='Include uppercase letters'
+											id='upper'
+											name='upper'
+											checked={config.upper}
+											onChange={() => dispatch({ type: 'upper' })}
+										/>
+									</div>
+									<div className='box'>
+										<Toggle
+											label='Include numbers'
+											id='numbers'
+											name='numbers'
+											checked={config.number}
+											onChange={() => dispatch({ type: 'number' })}
+										/>
+									</div>
+									<div className='box'>
+										<Toggle
+											label='Include symbols (!@#$%^&*)'
+											id='symbol'
+											name='symbol'
+											checked={config.symbol}
+											onChange={() => dispatch({ type: 'symbol' })}
+										/>
+									</div>
+								</div>
+							</section>
+						</form>
+					)}
+					{showHistory && (
+						<>
+							<div className='label'>History</div>
+							{history.map((item) => (
+								<div className='box box--history'>
+									<div
+										className='password'
+										dangerouslySetInnerHTML={{ __html: colorPassword(item) }}
 									/>
+									<div className='controls'>
+										<button type='button' onClick={() => navigator.clipboard.writeText(item)}>
+											<CopyIcon />
+										</button>
+									</div>
 								</div>
-								<div className='box'>
-									<Toggle
-										label='Include numbers'
-										id='numbers'
-										name='numbers'
-										checked={config.number}
-										onChange={() => dispatch({ type: 'number' })}
-									/>
-								</div>
-								<div className='box'>
-									<Toggle
-										label='Include symbols (!@#$%^&*)'
-										id='symbol'
-										name='symbol'
-										checked={config.symbol}
-										onChange={() => dispatch({ type: 'symbol' })}
-									/>
-								</div>
-							</div>
-						</section>
-					</form>
-				)}
-				{showHistory && (
-					<>
-						<div className='label'>History</div>
-						{history.map((item) => (
-							<div className='box box--password'>
-								<div
-									className='password'
-									dangerouslySetInnerHTML={{ __html: colorPassword(item) }}
-								/>
-								<div className='controls'>
-									<button type='button' onClick={() => navigator.clipboard.writeText(item)}>
-										<CopyIcon />
-									</button>
-								</div>
-							</div>
-						))}
-					</>
-				)}
-				<div className='app-controls'>
-					<button type='button' onClick={() => setShowHistory(false)}>
+							))}
+						</>
+					)}
+				</div>
+				<nav className='app-navigation'>
+					<button
+						className={!showHistory && 'active'}
+						type='button'
+						onClick={() => setShowHistory(false)}
+					>
 						Generator
 					</button>
-					<button type='button' onClick={() => setShowHistory(true)}>
+					<button
+						className={showHistory && 'active'}
+						type='button'
+						onClick={() => setShowHistory(true)}
+					>
 						History
 					</button>
-				</div>
+				</nav>
 			</div>
 		</div>
 	)
