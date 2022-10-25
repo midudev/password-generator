0d445212009e750248fc8009f52aab2ed4fb366e
eyubkh <eyub.kh@gmail.com>
2022-10-25 16:56:25 +0100

    adding css to page component and adding notification copied component

--- src/components/eyubkh/Page.jsx
+++ src/components/eyubkh/Page.jsx
@@ -1,11 +1,14 @@
 import { PasswordGenerator } from './PasswordGenerator'
 import { Castle } from './Castle'
+import { Copied } from './Copied'
+import './style/page.css'
 
 export default function Page() {
 	return (
-		<>
+		<main id='eyubkh-main'>
+			<Copied />
 			<PasswordGenerator />
 			<Castle />
-		</>
+		</main>
 	)
 }
