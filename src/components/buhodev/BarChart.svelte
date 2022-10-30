0f5ca3ff28d64e83a292cf320b9677f73e245f9d
Carlos Ivanchuk <buhodev@proton.me>
2022-10-30 16:38:41 -0400

    feat: calculate bar heighs dynamically

--- src/components/buhodev/BarChart.svelte
+++ src/components/buhodev/BarChart.svelte
@@ -1,8 +1,31 @@
-<script>
-	export const numberOfCharacters = [4, 15, 8, 2]
+<script lang="ts">
+	export let password: string
+
+	function getNumberOfDigits(str) {
+		return str.replace(/[^0-9]/g, '').length
+	}
+
+	function getNumberOfLowercase(str) {
+		return str.replace(/[^a-z]/g, '').length
+	}
+
+	function getNumberOfUppercase(str) {
+		return str.replace(/[^A-Z]/g, '').length
+	}
+
+	function getNumberOfSymbols(str) {
+		return str.replace(/[^`!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/g, '').length
+	}
 
 	function calculateHeight(charOcurrences, passwordLength) {
 		return (charOcurrences * 100) / passwordLength
+	}
+
+	$: numberOfCharacters = {
+		lowercase: getNumberOfLowercase(password),
+		uppercase: getNumberOfUppercase(password),
+		numbers: getNumberOfDigits(password),
+		symbols: getNumberOfSymbols(password)
 	}
 </script>
 
@@ -14,17 +37,17 @@
 	</div>
 	<div class="w-full">
 		<div class="relative flex px-4 gap-3 h-40 items-stretch w-full">
-			{#each numberOfCharacters as number, i}
+			{#each Object.keys(numberOfCharacters) as char, i}
 				<div class="flex flex-1 flex-col items-stretch justify-end">
-					{#if number <= 3}
-						<span class="text-center text-xs">{number}</span>
+					{#if numberOfCharacters[char] <= 3}
+						<span class="text-center text-xs">{numberOfCharacters[char]}</span>
 					{/if}
 					<div
 						class="bg-purple-500/80 border rounded-tl-sm rounded-tr-sm border-b-0 flex"
-						style="flex-basis: {calculateHeight(numberOfCharacters[i], 30)}%"
+						style="flex-basis: {calculateHeight(numberOfCharacters[char], 30)}%"
 					>
-						{#if number > 3}
-							<span class="w-full text-center text-xs">{number}</span>
+						{#if numberOfCharacters[char] > 3}
+							<span class="w-full text-center text-xs">{numberOfCharacters[char]}</span>
 						{/if}
 					</div>
 				</div>
