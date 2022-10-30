bfb69a0b955b5576ed4c52dd5cd9fa7d4708a868
Carlos Ivanchuk <buhodev@proton.me>
2022-10-29 22:49:02 -0400

    feat: add matrix effect

--- src/components/buhodev/Canvas.svelte
+++ src/components/buhodev/Canvas.svelte
@@ -1,4 +1,4 @@
-<script lang ts>
+<script lang='ts'>
 	import { onMount } from 'svelte'
 
 	export let width: number, height: number
@@ -13,6 +13,27 @@
 		canvas.width = width
 		ctx.fillStyle = '#000'
 		ctx.fillRect(0, 0, height, width)
+
+		const cols = Math.floor(width / 20) + 1
+		const ypos = Array(cols).fill(0)
+
+		function matrix() {
+			ctx.fillStyle = '#0001'
+			ctx.fillRect(0, 0, width, height)
+
+			ctx.fillStyle = '#0f0'
+			ctx.font = '15pt monospace'
+
+			ypos.forEach((y, index) => {
+				const text = String.fromCharCode(Math.random() * 128)
+				const x = index * 20
+				ctx.fillText(text, x, y)
+				if (y > 100 + Math.random() * 10000) ypos[index] = 0
+				else ypos[index] = y + 20
+			})
+		}
+
+		setInterval(matrix, 50)
 
 		function getMousePos(canvas, evt) {
 			var rect = canvas.getBoundingClientRect()
