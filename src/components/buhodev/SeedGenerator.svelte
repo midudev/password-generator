d7374afada08f4f9570434d6a47faebc170392a5
Carlos Ivanchuk <buhodev@proton.me>
2022-10-31 03:43:06 -0400

    fix: avoid large gaps

--- src/components/buhodev/SeedGenerator.svelte
+++ src/components/buhodev/SeedGenerator.svelte
@@ -13,7 +13,7 @@
 
 <div class="w-[300px]">
 	<span class="text-white mb-4 text-center inline-block w-full">Move your mouse over the green characters to generate the random seed.</span>
-	<textarea value={seed} readonly class="p-2 flex items-center h-24 justify-center text-white rounded-md bg-neutral-900/40 border border-neutral-700 backdrop-blur-sm text-2xl text-center font-mono overflow-x-auto" />
+	<textarea value={seed} readonly class="p-2 flex items-center break-all h-24 justify-center text-white rounded-md bg-neutral-900/40 border border-neutral-700 backdrop-blur-sm text-2xl text-center font-mono overflow-x-auto" />
 	<Progress value={$progress} />
 	<div class="mt-4 rounded-md overflow-hidden h-[300px] w-[300px]"><Canvas bind:seed width={300} height={300} /></div>
 </div>
