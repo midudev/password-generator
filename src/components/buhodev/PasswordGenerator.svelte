d4a7840452a86740101dc229f5a8a85918ea902c
Carlos Ivanchuk <buhodev@proton.me>
2022-10-31 17:27:37 -0400

    fix: add spacing & show tip only on generate view

--- src/components/buhodev/PasswordGenerator.svelte
+++ src/components/buhodev/PasswordGenerator.svelte
@@ -319,7 +319,7 @@
 </header>
 
 <div class="relative container max-w-full overflow-y-scroll">
-	<main class="max-w-md flex flex-col mx-auto text-white">
+	<main class="max-w-md flex flex-col mx-auto text-white pb-8">
 		{#if view === 'generate'}
 			<!-- Password -->
 			<button
@@ -798,6 +798,56 @@
 						</label>
 					{/each}
 				</div>
+
+				<div
+					class="m-4 mt-8 p-4 text-sm bg-neutral-600/10 space-x-1 flex text-neutral-300 rounded-md leading-none"
+				>
+					<span class="w-5 h-5 flex justify-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="inline-block w-5 h-5"
+							viewBox="0 0 24 24"
+							fill="none"
+						>
+							<path
+								d="M16.125 9.75 10.622 15l-2.747-2.625"
+								stroke="#fff"
+								stroke-width="1.5"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+							<path
+								d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Z"
+								stroke="#fff"
+								stroke-width="1.5"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+					</span>
+
+					<span class="leading-normal">
+						Tip: Click the copy
+						<span class="inline-block w-4"
+							><svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									class:text-blue-400={isCopied}
+									d={!isCopied
+										? 'M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z'
+										: 'M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75'}
+								/>
+							</svg></span
+						> button next to the password very fast to see what happens
+					</span>
+				</div>
 			{:else}
 				<div
 					in:fade={{ delay: 200 }}
@@ -807,56 +857,6 @@
 					Nothing to show here
 				</div>
 			{/if}
-
-			<div
-				class="m-4 p-4 text-sm bg-neutral-600/10 space-x-1 flex text-neutral-300 rounded-md leading-none"
-			>
-				<span class="w-5 h-5 flex justify-center">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="inline-block w-5 h-5"
-						viewBox="0 0 24 24"
-						fill="none"
-					>
-						<path
-							d="M16.125 9.75 10.622 15l-2.747-2.625"
-							stroke="#fff"
-							stroke-width="1.5"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						/>
-						<path
-							d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Z"
-							stroke="#fff"
-							stroke-width="1.5"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						/>
-					</svg>
-				</span>
-
-				<span class="leading-normal">
-					Tip: Click the copy
-					<span class="inline-block w-4"
-						><svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								class:text-blue-400={isCopied}
-								d={!isCopied
-									? 'M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z'
-									: 'M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75'}
-							/>
-						</svg></span
-					> button next to the password very fast to see what happens
-				</span>
-			</div>
 
 			<span class="text-center mt-auto mb-2"
 				>Made by <a
